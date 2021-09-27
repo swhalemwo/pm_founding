9d977c14e574c424f7adb67d19314ee95301d30a
Johannes Aengenheyster <johannes.ae@hotmail.de>
2021-09-27 16:33:14 +0200

    get country codes working, just atm yeet out everything that doesn't fit

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -1,6 +1,7 @@
 library("readxl")
 library(tibble)
 library(reshape2)
+library(dplyr)
 
 df <- read_excel("/home/johannes/Dropbox/phd/papers/org_pop/data/Private museum database.xlsx")
 ## removing header stuff 
@@ -8,7 +9,7 @@ nrows <- nrow(df)-1
 df <- df[2:nrows,]
 
 
-tbl <- table(df$Country)
+# tbl <- table(df$Country)
 
 df$country <- df$"Country where museum is located"
 
@@ -18,16 +19,21 @@ df$year_closed <- df$"Closing year"
 df$museum_closed <- df$"Museum closed"
 
 
-tbl <- table(df$country)
-tbl2 <- tbl[rev(order(tbl))]
-tbl2
+## tbl <- table(df$country)
+## tbl2 <- tbl[rev(order(tbl))]
+## tbl2
 
 
 df[which(df$country == "USA"),]$country <- "United States"
 df[which(df$country == "Missouri"),]$country <- "United States"
+df[which(df$country == "England"),]$country <- "United Kingdom"
+
+
+df$countrycode <- recode(df$country, "United Kingdom" = "GBR", "Spain" = "ESP", "United States" = "USA", "Switzerland" = "CHE" , "India" = "IND", "Greece" = "GRC", "Lebanon" = "LBN", "France" = "FRA", "Estonia" = "EST", "Azerbaijan" = "AZE", "Latvia" = "LVA", "Madagascar" = "MDG", "Indonesia" = "IDN", "Slovakia" = "SVK", "Romania" = "ROU","Argentina" = "ARG","South Korea" = "KOR", "Japan" = "JPN", "Benin" = "BEN", "Bangladesh" = "BGD", "Australia" = "AUS", "Norway" = "NOR", "New Zealand" = "NZL", "Poland" = "POL", "Nigeria" = "NGA", "Portugal" = "PRT", "Serbia" = "SRB","Czech Republic" = "CZE","Senegal" = "SEN", "Puerto Rico" = "PRI", "Taiwan" = "TWN", "Israel" = "ISR", "England" = "GBR", "China" = "CHN", "Germany" = "DEU", "Netherlands" = "NLD", "Italy" = "ITA", "Russia" = "RUS", "Canada" = "CAN", "Hungary" = "HUN", "Belgium" = "BEL", "Sweden" = "SWE", "Finland" = "FIN","Malaysia" = "MYS","Philippines" = "PHL", "Turkey" = "TUR", "Austria" = "AUT", "South Africa" = "ZAF","Thailand" = "THA", "Denmark" = "DNK",  "Mexico" = "MEX", "United Arab Emirates" = "ARE","Brazil" = "BRA", "Hong Kong" = "HKG", "Ukraine" = "UKR", "Kuwait" = "KWT",  "Cyprus" = "CYP", "Monaco" = "MCO", "Iceland" = "ISL", "Kenya" = "KEN", "Singapore" = "SGP", "Iran" = "IRN", "Lithuania" = "LTU", .default= NA_character_)
 
 
 ## ** basic table preparation
+## add year: 
 ## just clean everything by taking first 4, drop everything that doesn't work lmao
 df$year_opened_int <- as.integer(lapply(df$year_opened, function(x)(substring(x, 0,4))))
 ## have 59 NAs, oh well
@@ -39,26 +45,29 @@ df$year_opened_int2 <- df$year_opened_int
 df$year_opened_int2[which(df$year_opened_int2 < 1985 | df$year_opened_int2 > 2021)] <- NA
 ## up to 100 dropped atm 
 
-df_open <- na.omit(df[,c('name', 'country', 'year_opened_int2')])
+df_open <- na.omit(df[,c('name', 'country', 'countrycode', 'year_opened_int2')])
 # plot(table(df_open$year_opened_int2), type='l')
 df_open$ctr <- 1
 
-df_country_year_agg <- as_tibble(aggregate(df_open$ctr, by=list(df_open$country, df_open$year_opened_int2), FUN = sum))
-names(df_country_year_agg) <- c('country', 'year', 'nbr_opened')
+df_country_year_agg <- as_tibble(aggregate(df_open$ctr, by=list(df_open$country, df_open$countrycode, df_open$year_opened_int2), FUN = sum))
 
-
+names(df_country_year_agg) <- c('country', 'countrycode', 'year', 'nbr_opened')
 
 
 ## make some combinations with countries x years join what I have
-df_country_years_empty <- as_tibble(expand.grid(unique(df_open$country), unique(df_open$year_opened_int2)))
-names(df_country_years_empty) <- c('country', 'year')
+df_country_years_empty <- as_tibble(expand.grid(unique(df_open$countrycode), unique(df_open$year_opened_int2)))
+names(df_country_years_empty) <- c('countrycode', 'year')
+df_country_years_empty$countrycode <- as.character(df_country_years_empty$countrycode)
+
+
+df_country_years_empty2 <- as_tibble(merge(df_country_years_empty, unique(df_open[,c('country', 'countrycode')]) , by='countrycode', all.x = TRUE))
+
 
 ## join everything nice together
-df_country_years <- as_tibble(merge(df_country_year_agg, df_country_years_empty, by = c('country', 'year'), all=TRUE))
+df_country_years <- as_tibble(merge(df_country_year_agg, df_country_years_empty2, by = c('country', 'countrycode', 'year'), all=TRUE))
 
 ## fill up NAs with 0s
 df_country_years$nbr_opened[which(is.na(df_country_years$nbr_opened))] <- 0
-
 
 
 ## ** read in some wb gdp data for basic testing
@@ -93,9 +102,15 @@ df_anls <- as_tibble(merge(df_country_years, df_gdp_pcap_molt, by=c('country', '
 na_agg_cnt <- aggregate(df_anls$gdp_pcap, list(df_anls$country), is.na)
 
 aggregate(gdp_pcap ~ country, data = df_anls, function(x){sum(is.na(x))}, na.action = NULL)
-## crappy names not the same
+## crappy, names not the same
+## seems best to use country codes, easier across different databases
 
 
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+unique(df$countrycode)[which(unique(df$countrycode) %!in% (unique(df_gdp_pcap_molt$countrycode)))]
+## seems ok,
+## taiwan not separate country in WB.. just 1 PM tho, so shouldn't be big impact
 
 
 
