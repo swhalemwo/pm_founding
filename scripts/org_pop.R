538cbf453eb2427b37b378d6edde4840a073bd5d
Johannes Aengenheyster <johannes.ae@hotmail.de>
2021-10-01 11:21:51 +0200

    move negative binomial tests around to have only one nb section

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -226,40 +226,6 @@ screenreg(found.fe)
 screenreg(list(found.fe,found.fe_wo_gini))
 
 
-## ** negative binomial
-## *** example
-## https://rdrr.io/cran/lme4/man/glmer.nb.html
-
-set.seed(101)
-dd <- expand.grid(f1 = factor(1:3),
-                  f2 = LETTERS[1:2], g=1:9, rep=1:15,
-          KEEP.OUT.ATTRS=FALSE)
-summary(mu <- 5*(-4 + with(dd, as.integer(f1) + 4*as.numeric(f2))))
-dd$y <- rnbinom(nrow(dd), mu = mu, size = 0.5)
-str(dd)
-require("MASS")## and use its glm.nb() - as indeed we have zero random effect:
-## Not run: 
-m.glm <- glm.nb(y ~ f1*f2, data=dd, trace=TRUE)
-summary(m.glm)
-m.nb <- glmer.nb(y ~ f1*f2 + (1|g), data=dd, verbose=TRUE)
-m.nb
-## The neg.binomial theta parameter:
-getME(m.nb, "glmer.nb.theta")
-LL <- logLik(m.nb)
-## mixed model has 1 additional parameter (RE variance)
-stopifnot(attr(LL,"df")==attr(logLik(m.glm),"df")+1)
-plot(m.nb, resid(.) ~ g)# works, as long as data 'dd' is found
-
-
-## *** application
-found.nb <- glmer.nb(nbr_opened ~ nbr_opened_lag1 + (1 | countrycode), data = df_anls)
-found.nb1 <- glmer.nb(nbr_opened ~ log(gdp_pcap_lag1) + (1 | countrycode), data = df_anls)
-screenreg(found.nb1)
-plot(found.nb1)
-
-
-
-
 ## ** poisson
 
 ## *** glmer (lme4)
@@ -339,6 +305,30 @@ screenreg(list(found.pglm.poi1,found.poi1,
 
 
 ## ** negative binomial
+
+## *** example
+## https://rdrr.io/cran/lme4/man/glmer.nb.html
+
+set.seed(101)
+dd <- expand.grid(f1 = factor(1:3),
+                  f2 = LETTERS[1:2], g=1:9, rep=1:15,
+          KEEP.OUT.ATTRS=FALSE)
+summary(mu <- 5*(-4 + with(dd, as.integer(f1) + 4*as.numeric(f2))))
+dd$y <- rnbinom(nrow(dd), mu = mu, size = 0.5)
+str(dd)
+require("MASS")## and use its glm.nb() - as indeed we have zero random effect:
+## Not run: 
+m.glm <- glm.nb(y ~ f1*f2, data=dd, trace=TRUE)
+summary(m.glm)
+m.nb <- glmer.nb(y ~ f1*f2 + (1|g), data=dd, verbose=TRUE)
+m.nb
+## The neg.binomial theta parameter:
+getME(m.nb, "glmer.nb.theta")
+LL <- logLik(m.nb)
+## mixed model has 1 additional parameter (RE variance)
+stopifnot(attr(LL,"df")==attr(logLik(m.glm),"df")+1)
+plot(m.nb, resid(.) ~ g)# works, as long as data 'dd' is found
+
 
 ## *** pglm
 
