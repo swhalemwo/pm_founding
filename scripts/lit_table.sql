c3da298a2f14da33a2ad2135b8ce036d5c00ce94
Johannes Aengenheyster <johannes.ae@hotmail.de>
2021-11-05 17:25:39 +0100

    some basic CH queries, first for lit table, now for DOTS

---
+++ scripts/lit_table.sql
@@ -0,0 +1,36 @@
+set joined_subquery_requires_alias=0
+
+set output_format_pretty_max_rows=200
+
+show tables
+
+-- SELECT * FROM bc WHERE parent='interna'
+
+-- SELECT * FROM bc WHERE child='DeCastro_2018_inhotim'
+
+-- SELECT parent, COUNT(parent) AS cnt FROM (
+--   SELECT child, parent FROM (
+--     SELECT DISTINCT(child) AS child FROM bc WHERE parent IN ['ecology', 'legitimation', 'competition', 'founding', 'contagion', 'diffusion', 'density']
+--   ) JOIN (SELECT parent, child FROM bc WHERE parent NOT IN ['cls_papers', 'cls_toread', 'sbcls_B', 'sbcls_A', 'sbcls_C', 'cls_orgform']) USING child) GROUP BY parent ORDER BY cnt DESC
+
+  
+
+  
+  select count(distinct(concat(country_name, '-', counterpart_country_name))) from dots_prep where time_period='2020' limit 10
+
+SELECT COUNT(DISTINCT(CONCAT(country_name, '-', counterpart_country_name))) FROM dots_prep WHERE time_period='1985' 
+
+
+SELECT DISTINCT(CONCAT(country_name, '-', counterpart_country_name)) AS pair FROM dots_prep WHERE time_period='1985'
+
+SELECT DISTINCT(CONCAT(country_name, '-', counterpart_country_name)) AS pair FROM dots_prep WHERE time_period='2020'
+
+
+
+
+
+SELECT pair FROM (
+  SELECT DISTINCT(CONCAT(country_name, '-', counterpart_country_name)) AS pair FROM dots_prep WHERE time_period='1985') LEFT JOIN (
+    SELECT DISTINCT(CONCAT(country_name, '-', counterpart_country_name)) AS pair FROM dots_prep WHERE time_period='2020') USING pair
+
+
