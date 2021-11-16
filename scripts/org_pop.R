2cbc6b3253b051928b05b98bb7902d03c37ec8fd
Johannes Aengenheyster <johannes.ae@hotmail.de>
2021-11-16 12:36:27 +0100

    finish/systematize WID wealth variables coverage check

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -15,7 +15,7 @@ library(docstring)
 library(DBI)
 library(TTR)
 library(ggpubr)
-
+library(xtable)
 ds <- docstring
 
 
@@ -631,51 +631,6 @@ check_wid_cpltns <- function(varx, percentile){
 ## could add something like how complete the countries are that are not fully complete to assess how difficult imputation will be 
 
 
-wid_wealth_vars_cmd <- "SELECT DISTINCT(variable), percentile FROM wdi WHERE ilike(varx, '%weal%' )"
-wid_wealth_vars <- as_tibble(dbGetQuery(con, wid_wealth_vars_cmd))
-unique(wid_wealth_vars)
-
-check_wid_cpltns("mgweal999i", "p0p100")
-
-
-combos <- split(wid_wealth_vars, seq(nrow(wid_wealth_vars)))
-
-wid_cpltns_check_wrapper <- function(combo){
-    print(combo)
-    return(check_wid_cpltns(combo$variable, combo$percentile))
-}
-
-
-check_wid_cpltns(combos[[1]][[1]][1], combos[[1]][[1]][2])
-wid_cpltns_check_wrapper(combos[[1]])
-
-wealth_res <- mclapply(combos, wid_cpltns_check_wrapper, mc.cores = 6)
-wealth_res_df <- as_tibble(apply(Reduce(function(x,y,...) rbind(x,y,...), wealth_res), 2, unlist))
-## sloppy converting numbers back to numeric
-wealth_res_df[c("PMs_covered_raw", "cry_cvrg_geq3", "nbr_of_crys_geq3", "nbr_of_crys_geq1pm")] <- apply(wealth_res_df[c("PMs_covered_raw", "cry_cvrg_geq3", "nbr_of_crys_geq3", "nbr_of_crys_geq1pm")], 2, as.numeric)
-
-
-wealth_res_df2 <- as.data.frame(filter(wealth_res_df, PMs_covered_raw > 250))[,-c(which(names(wealth_res_df) == "most_affected_crys"))]
-
-wealth_res_df2$variable_label <- recode(wealth_res_df2$variable,
-      "apweal999i" = "average individual wealth of combined sector (households, NPISH)",
-      "anweal999i" = "average individual wealth of national economy",
-      "wwealn999i" = "wealth-to-income ratio of national economy",
-      "mpweal999i" = "total wealth of combined sector",
-      "apweal992i" = "average individual wealth of combined sector (above 20 y/o)",
-      "mgweal999i" = "total net wealth of general government",
-      "wwealg999i" = "wealth-to-income ratio of national economy",
-      "wwealp999i" = "net private wealth to net national income ratio",
-      "anweal992i" = "average individual wealth of national economy (above 20 y/o)",
-      "agweal999i" = "average individual net wealth of general government",
-      "mnweal999i" = "total individual wealth of national economy",
-      "agweal992i" = "average individual net wealth of general government")
-
-
-
-
-
-
 
 
 
@@ -742,7 +697,7 @@ if (REDO_WID_CPLTNS_CHK){
 
     
 
-    library(xtable)
+
     print(
         xtbl,
         include.rownames = F,
@@ -750,6 +705,72 @@ if (REDO_WID_CPLTNS_CHK){
         )
 
 }
+
+REDO_WID_WEALTH_CHECKS <- FALSE
+if (REDO_WID_WEALTH_CHECKS){
+
+    wid_wealth_vars_cmd <- "SELECT DISTINCT(variable), percentile FROM wdi WHERE ilike(varx, '%weal%' )"
+    wid_wealth_vars <- as_tibble(dbGetQuery(con, wid_wealth_vars_cmd))
+    unique(wid_wealth_vars)
+
+    check_wid_cpltns("mgweal999i", "p0p100")
+
+
+    combos <- split(wid_wealth_vars, seq(nrow(wid_wealth_vars)))
+
+    wid_cpltns_check_wrapper <- function(combo){
+        print(combo)
+        return(check_wid_cpltns(combo$variable, combo$percentile))
+    }
+
+
+    check_wid_cpltns(combos[[1]][[1]][1], combos[[1]][[1]][2])
+    wid_cpltns_check_wrapper(combos[[1]])
+
+    wealth_res <- mclapply(combos, wid_cpltns_check_wrapper, mc.cores = 6)
+    wealth_res_df <- as_tibble(apply(Reduce(function(x,y,...) rbind(x,y,...), wealth_res), 2, unlist))
+    ## sloppy converting numbers back to numeric
+    wealth_res_df[c("PMs_covered_raw", "cry_cvrg_geq3", "nbr_of_crys_geq3", "nbr_of_crys_geq1pm")] <- apply(wealth_res_df[c("PMs_covered_raw", "cry_cvrg_geq3", "nbr_of_crys_geq3", "nbr_of_crys_geq1pm")], 2, as.numeric)
+
+
+    wealth_res_df2 <- as.data.frame(filter(wealth_res_df, PMs_covered_raw > 250))[,-c(which(names(wealth_res_df) %in% c("most_affected_crys", "percentile")))]
+
+    wealth_res_df2$variable_label <- recode(wealth_res_df2$variable,
+                                            "apweal999i" = "average individual wealth of combined sector (households, NPISH)",
+                                            "anweal999i" = "average individual wealth of national economy",
+                                            "wwealn999i" = "wealth-to-income ratio of national economy",
+                                            "mpweal999i" = "total wealth of combined sector",
+                                            "apweal992i" = "average individual wealth of combined sector (above 20 y/o)",
+                                            "mgweal999i" = "total net wealth of general government",
+                                            "wwealg999i" = "wealth-to-income ratio of national economy",
+                                            "wwealp999i" = "net private wealth to net national income ratio",
+                                            "anweal992i" = "average individual wealth of national economy (above 20 y/o)",
+                                            "agweal999i" = "average individual net wealth of general government",
+                                            "mnweal999i" = "total individual wealth of national economy",
+                                            "agweal992i" = "average individual net wealth of general government")
+
+    wealth_res_df2 <- wealth_res_df2[,c('variable', 'variable_label', 'PMs_covered_raw', 'cry_cvrg_geq3', 'nbr_of_crys_geq3', 'nbr_of_crys_geq1pm')]
+
+    names(wealth_res_df2) <- c("variable", "meaning", "PM foundings\n covered directly", "PM foundings in countries with data for at least 3 years", "number of countries with data for at least 3 years", "number of countries with data and at least 1 PM founding")
+
+
+    xtbl <- xtable(
+        wealth_res_df2,
+        label="wid_cpltns",
+        caption = "coverage variables for top decile in WID",
+        align = c("p{2cm}", "l", "p{5.5cm}","p{1.5cm}", rep("p{2.25cm}", 3)),
+        digits=0)
+
+
+
+    print(
+        xtbl,
+        include.rownames = F,
+        file = paste0(TABLE_DIR, "wid_wealth_cpltns.tex"),
+        )
+}
+
+
                     
 ## **** check whether coverage depends on percentile chosen, doesn't really
 
