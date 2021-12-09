b93d4135f8b7601b2b4c47348900f300e196a11e
Johannes Aengenheyster <johannes.ae@hotmail.de>
2021-12-09 17:19:02 +0100

    write custom rolling mean function that doesn't change vector length

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -76,6 +76,78 @@ lm.beta.lmer <- function(mod) {
 }
 
 
+minus <- function(a,b){return(a-b)}
+plus <- function(a,b){return(a+b)}
+
+rollmean_custom <- function(v, win_len, func, orientation){
+    c <- 1
+    ## slider needs orientation if length of sliding window is even:
+    ## question is whether to orient oneself to the right of left
+    len_v <- length(v)
+
+    orientation <- "left"
+
+    all_res <- c()
+    
+    for (i in v){
+        ## select indices
+        ## just assume left, win_len=2
+        ## indices <- c(c, c-1)
+        indices <- c(c)
+
+        ## initializing the operation to perform based on orientation
+        if (orientation == "left"){
+            op <- minus
+            op_name <- "minus"
+        } else {
+            op <- plus
+            op_name <- "plus"
+        }
+        for (k in seq(win_len-1)){
+            ## need -1 because first value c is already in indices
+            last_pos <- tail(indices,1)
+            
+            indices <- c(indices, op(last_pos,k))
+
+            ## switching the operation
+            if (op_name == "minus") {
+                op <- plus
+                op_name <- "plus"
+            } else {
+                op <- minus
+                op_name <- "minus"
+            }
+        }
+        
+        indices_sel <- indices[intersect(which(indices>=1), which(indices <= len_v))]
+        ## print(paste0("indices_sel: ", indices_sel))
+        values <- v[c(indices_sel)]
+
+        win_res <- mean(values)
+        all_res <- c(all_res, win_res)
+        c <- c+1
+    }
+    return(all_res)
+}
+
+## rollmean_custom(x, win_len = 3, orientation = "left")
+
+## x <- seq(1,100)/10
+## x1 <- sin(x)
+
+## x2 <- sin(seq(1,100))
+## x2 <- rnorm(100, 0,1)
+## plot(x2)
+
+## plot(x1+x2)
+
+## lines(rollmean_custom(x1+x2, win_len = 10, orientation = "left"))
+## lines(rollmean_custom(x1+x2, win_len = 15, orientation = "left"))
+## lines(rollmean_custom(x1+x2, win_len = 5, orientation = "left"))
+## lines(rollmean_custom(x1+x2, win_len = 2, orientation = "left"))
+## lines(rollmean_custom(x1+x2, win_len = 3, orientation = "left"))
+
+
 ## ** read in some wb gdp data for basic testing
 ## *** gdp_pcap: gdp per capita
 ## probably need in long_format
