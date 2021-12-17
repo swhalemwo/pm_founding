d717d9f270a3331aa2c1a57bcf4223591687b357
Johannes Aengenheyster <johannes.ae@hotmail.de>
2021-12-17 18:55:48 +0100

    parse grepped result data, download corresponding OECD datasets

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -16,6 +16,8 @@ library(DBI)
 library(TTR)
 library(ggpubr)
 library(xtable)
+library(OECD)
+library(rsdmx)
 ds <- docstring
 
 
@@ -2789,6 +2791,179 @@ proc_sdmx_file <- function(sdmx_file){
 }
 
 mclapply(files_there, proc_sdmx_file, mc.cores = 6)
+
+## ** working with parsing results
+relevant_sdmx_tables <- c(
+    "AEA---CL_AEA_ACTIVITY.csv",
+    "BIMTS_CPA---CL_BIMTS_CPA_CPA_VER_2_1.csv",
+    "DIOC_OCCUPATION_DET---CL_DIOC_OCCUPATION_DET_DET_OCCUP.csv",
+    "FATS_OUT3_SERV---CL_FATS_OUT3_SERV_SERV.csv",
+    "FDI_CTRY_IND_SUMM---CL_FDI_CTRY_IND_SUMM_ECO_ACT.csv",
+    "FDI_INC_IND---CL_FDI_INC_IND_ECO_ACT.csv",
+    "FDI_POS_IND---CL_FDI_POS_IND_ECO_ACT.csv",
+    "ERTR_ACC---CL_ERTR_ACC_ACT.csv",
+    "FDI_FLOW_IND---CL_FDI_FLOW_IND_ECO_ACT.csv",
+    "FATS_IN3_SERV---CL_FATS_IN3_SERV_SERV.csv",
+    "FDI_CTRY_ECO_HIST---CL_FDI_CTRY_ECO_HIST_ECO_ACT.csv",
+    "NCM_LIVE---CL_NCM_LIVE_INDICATOR.csv",
+    "NCM_STAGING---CL_NCM_STAGING_INDICATOR.csv",
+    "SNA_TABLE42---CL_SNA_TABLE42_ACTIVITY.csv",
+    "SNA_TABLE8A_ARCHIVE---CL_SNA_TABLE8A_ARCHIVE_ACTIVITY.csv",
+    "SNA_TABLE31---CL_SNA_TABLE31_ACTIVITY.csv",
+    "SNA_TABLE44---CL_SNA_TABLE44_TRANSACT.csv",
+    "SNA_TABLE44---CL_SNA_TABLE44_PRODUCT.csv",
+    "SNA_TABLE6A_ARCHIVE---CL_SNA_TABLE6A_ARCHIVE_ACTIVITY.csv",
+    "SDBS_BDI---CL_SDBS_BDI_SEC.csv",
+    "SNA_TABLE7A_SNA93---CL_SNA_TABLE7A_SNA93_ACTIVITY.csv",
+    "TEC1_REV4_COPY---CL_TEC1_REV4_COPY_SECTOR.csv",
+    "SNA_TABLE8A---CL_SNA_TABLE8A_ACTIVITY.csv",
+    "TEC5_REV4---CL_TEC5_REV4_SECTOR.csv",
+    "SSIS_BSC---CL_SSIS_BSC_ISIC3.csv",
+    "STANI4_2020---CL_STANI4_2020_IND.csv",
+    "SNA_TABLE40---CL_SNA_TABLE40_TRANSACT.csv",
+    "SNA_TABLE40---CL_SNA_TABLE40_PRODUCT.csv",
+    "SNA_TABLE6A---CL_SNA_TABLE6A_ACTIVITY.csv",
+    "SNA_TABLE30---CL_SNA_TABLE30_TRANSACT.csv",
+    "SNA_TABLE30---CL_SNA_TABLE30_PRODUCT.csv",
+    "SNA_TABLE43---CL_SNA_TABLE43_TRANSACT.csv",
+    "SNA_TABLE43---CL_SNA_TABLE43_PRODUCT.csv",
+    "SNA_TABLE7A_ARCHIVE---CL_SNA_TABLE7A_ARCHIVE_ACTIVITY.csv",
+    "TEC9_REV4---CL_TEC9_REV4_SECTOR.csv",
+    "SNA_TABLE9A---CL_SNA_TABLE9A_ACTIVITY.csv",
+    "SSIS_BSC_ISIC4---CL_SSIS_BSC_ISIC4_ISIC4.csv",
+    "STANI4_2016---CL_STANI4_2016_IND.csv",
+    "SNA_TABLE41---CL_SNA_TABLE41_ACTIVITY.csv",
+    "SDBS_BDI_ISIC4---CL_SDBS_BDI_ISIC4_SEC.csv",
+    "TEC1_REV4---CL_TEC1_REV4_SECTOR.csv",
+    "TEC6_REV4---CL_TEC6_REV4_SECTOR.csv",
+    "SNA_TABLE45---CL_SNA_TABLE45_ACTIVITY.csv",
+    "SNA_TABLE45---CL_SNA_TABLE45_PRODUCT.csv",
+    "SNA_TABLE7A---CL_SNA_TABLE7A_ACTIVITY.csv",
+    "STANI4---CL_STANI4_IND.csv",
+    "STANINDICATORSI4---CL_STANINDICATORSI4_IND.csv")
+
+
+sdmx_res_tbls <- lapply(relevant_sdmx_tables, function(x) as_tibble(read.csv(paste0(SDMX_TBL_DIR, x))))
+
+sdmx_res_tbl_names <- unlist(lapply(sdmx_res_tbls, names))
+table(sddmx_res_tbl_names)
+
+col_names <- c("sdmx_id", "codelist", "id", "label.en", "description.en")
+
+sdmx_res_cbn <- as_tibble(Reduce(function(x,y, ...) rbind(x[,col_names],y[,col_names]),sdmx_res_tbls))
+
+## grepping multiple columns works best with apply, lapply on names(df) doesn't work properly for some reason 
+sdmx_res_fltrd <- sdmx_res_cbn[which(rowSums(apply(sdmx_res_cbn, 2, function(x) grepl("museum", x)))>0),]
+sdmx_res_fltrd$id
+
+sdmx_res_fltrd[28,]
+## hello "D90T92" my old friend
+## let's see how to query you
+
+x <- as_tibble(get_dataset("STANI4_2020", filter = list("D90T92")))
+
+## maybe possible to first search which column is actually the indicator?
+
+unlist(lapply(names(df_stan), function(x) grepl("D90T92", df_stan[,x])))
+## for STAN, D90T92 is in the first column 
+
+## trying to pass all for countries, just get specific indicator
+x <- as_tibble(get_dataset("STANI4_2020", filter=list(c("AUTx"),c("PROD"), c("D90T92"))))
+## this works for some reason
+
+## passing all doesn't work tho :(
+x <- as_tibble(get_dataset("STANI4_2020", filter=list(c("all"),c("PROD"), c("D90T92"))))
+
+x <- as_tibble(get_dataset("STANI4_2020", filter="PROD.AUT", pre_formatted = TRUE))
+
+## working url: https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/STANI4_2020/AUT.PROD.D90T92/all
+## not working: https://stats.oecd.org/restsdmx/sdmx.ashx/GetData/STANI4_2020/all.PROD.D90T92/all
+
+## https://data.oecd.org/api/sdmx-json-documentation/: CAN USE EMPTY STRING TO SELECT ALL
+x <- as_tibble(get_dataset("STANI4_2020", filter=list(c(""),c("PROD"), c("D90T92"))))
+x <- as_tibble(get_dataset("STANI4_2020", filter=list(c(""),c(""), c("D90T92"))))
+
+
+## AEA actually has the same structure huh 
+x2 <- as_tibble(get_dataset("AEA", filter = list(c(""),c(""), c("R90-R92"))))
+
+## FDI_POS_IND: doesn't have the same structure
+## adding/removing empty filter strings doesn't seem to work
+
+x3 <- as_tibble(get_dataset("FDI_POS_IND", filter = list(c(""),c(""), c("R91"))))
+## actually does when just adding enough LUL 
+x3 <- as_tibble(get_dataset("FDI_POS_IND", filter = list(c(""),c(""),c(""), c(""), c(""), c(""),  c(""), c(""), c("R91"))))
+
+
+OECD_DATA_DIR <- "/home/johannes/ownCloud/oecd/api_data/"
+
+download_oecd_df <- function(datasetx, filter_list){
+    dfx <- get_dataset(datasetx, filter=filter_list)
+    write.csv(dfx, paste0(OECD_DATA_DIR, datasetx))
+    print("success")
+    done <- TRUE
+    }
+
+download_oecd_df(datasetx, list(c(""), c(""), c("D90T92")))
+download_oecd_df(datasetx, list((idx)))
+
+idx <- "D90T92"
+datasetx <- "STANI4_2020"
+
+idx <- "923"
+datasetx <- "SDBS_BDI"
+
+datasetx <- sdmx_res_fltrd[2,]$sdmx_id
+idx <- sdmx_res_fltrd[2,]$id
+
+datasets_already_there <- list.files(OECD_DATA_DIR)
+
+
+options(timeout = 180)
+
+download_oecd_dataset <- function(datasetx, idx) {
+    print(paste0("dataset: ", datasetx, " id: ", idx))
+    if (datasetx %!in% datasets_already_there){
+
+        done <- FALSE
+        filter_list <- list(c(idx))
+
+        while (done==FALSE) {
+            res <- tryCatch({
+                print(paste0(filter_list, collapse = "-"))
+                download_oecd_df(datasetx, filter_list)
+                done <- TRUE
+            },
+            error=function(e) {
+                print(paste0("error: ", length(filter_list)))
+                filter_list <- rev(append(rev(filter_list), ""))
+            })
+            filter_list <- res
+            Sys.sleep(2)
+        }
+    }
+
+}      
+
+    
+apply(sdmx_res_fltrd, 1, function(x) download_oecd_dataset(x["sdmx_id"], x["id"]))
+
+
+test_printer <- function(datasetx, idx){
+    print(paste0("datasetx: ", datasetx))
+    print(paste0("idx: ", idx))
+    print("-----------")
+    }
+
+apply(sdmx_res_fltrd, 1, test_printer, datasetx=sdmx_id, idx=id)
+
+apply(sdmx_res_fltrd, 1, function(x) test_printer(x$sdmx_id, x$id))
+apply(sdmx_res_fltrd, 1, function(x) test_printer(x["sdmx_id"], x["id"]))
+
+test_printer("asdf", "jjj")
+
+apply(sdmx_res_fltrd, 1, function(x) print(names(x)))
+apply(sdmx_res_fltrd, 1, function(x) print(x["sdmx_id"]))
 
 
 
