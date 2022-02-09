bff4b41882d2a33292a4764f39945473125b21cb
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-02-09 11:45:26 +0100

    functionalize basic excel to df process, add very limited data diagnostics

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -25,36 +25,69 @@ ds <- docstring
 options(show.error.messages = TRUE)
 options(show.error.locations = TRUE)
 
+PMDB_DIR <- "/home/johannes/Dropbox/phd/papers/org_pop/data/pmdb/"
 
-df <- read_excel("/home/johannes/Dropbox/phd/papers/org_pop/data/Private museum database.xlsx")
-df <- read_excel("/home/johannes/Dropbox/phd/papers/org_pop/data/Private museum database2.xlsx")
-## removing header stuff 
-nrows <- nrow(df)-1
-df <- df[2:nrows,]
+create_base_df <- function() {
 
+    ## df <- read_excel("/home/johannes/Dropbox/phd/papers/org_pop/data/Private museum database.xlsx")
+    ## df <- read_excel("/home/johannes/Dropbox/phd/papers/org_pop/data/Private museum database2.xlsx")
 
-## tbl <- table(df$Country)
-
-df$country <- df$"Country where museum is located"
-
-df$name <- df$Museumname
-df$year_opened <- df$"Opening year"
-df$year_closed <- df$"Closing year"
-df$museum_closed <- df$"Museum closed"
+    ## 2: use more recent version
+    df <- read_excel(paste0(PMDB_DIR, "Private museum database2.xlsx"))
+    
+    ## removing header stuff 
+    nrows <- nrow(df)-1
+    df <- df[2:nrows,]
 
 
-## tbl <- table(df$country)
-## tbl2 <- tbl[rev(order(tbl))]
-## tbl2
+    df$country <- df$"Country where museum is located"
+
+    df$name <- df$Museumname
+    df$year_opened_str <- df$"Opening year"
+    df$year_closed <- df$"Closing year"
+    df$museum_closed <- df$"Museum closed"
+
+    ## tbl <- table(df$country)
+    ## tbl2 <- tbl[rev(order(tbl))]
+    ## tbl2
+
+    df[which(df$country == "England"),]$country <- "United Kingdom"
 
 
+    df$countrycode <- recode(df$country, "United Kingdom" = "GBR", "Spain" = "ESP", "United States" = "USA", "Switzerland" = "CHE" , "India" = "IND", "Greece" = "GRC", "Lebanon" = "LBN", "France" = "FRA", "Estonia" = "EST", "Azerbaijan" = "AZE", "Latvia" = "LVA", "Madagascar" = "MDG", "Indonesia" = "IDN", "Slovakia" = "SVK", "Romania" = "ROU","Argentina" = "ARG","South Korea" = "KOR", "Japan" = "JPN", "Benin" = "BEN", "Bangladesh" = "BGD", "Australia" = "AUS", "Norway" = "NOR", "New Zealand" = "NZL", "Poland" = "POL", "Nigeria" = "NGA", "Portugal" = "PRT", "Serbia" = "SRB","Czech Republic" = "CZE","Senegal" = "SEN", "Puerto Rico" = "PRI", "Taiwan" = "TWN", "Israel" = "ISR", "England" = "GBR", "China" = "CHN", "Germany" = "DEU", "Netherlands" = "NLD", "Italy" = "ITA", "Russia" = "RUS", "Canada" = "CAN", "Hungary" = "HUN", "Belgium" = "BEL", "Sweden" = "SWE", "Finland" = "FIN","Malaysia" = "MYS","Philippines" = "PHL", "Turkey" = "TUR", "Austria" = "AUT", "South Africa" = "ZAF","Thailand" = "THA", "Denmark" = "DNK",  "Mexico" = "MEX", "United Arab Emirates" = "ARE","Brazil" = "BRA", "Hong Kong" = "HKG", "Ukraine" = "UKR", "Kuwait" = "KWT",  "Cyprus" = "CYP", "Monaco" = "MCO", "Iceland" = "ISL", "Kenya" = "KEN", "Singapore" = "SGP", "Iran" = "IRN", "Lithuania" = "LTU", .default= NA_character_)
 
-df[which(df$country == "USA"),]$country <- "United States"
-df[which(df$country == "Missouri"),]$country <- "United States"
-df[which(df$country == "England"),]$country <- "United Kingdom"
+    ## debugging unclear/missing countries
+    # filter(df, is.na(countrycode))[,c("country", "name")]
 
+    df$year_opened_int <- as.integer(lapply(df$year_opened_str, function(x)(substring(x, 0,4))))
+    ## as.data.frame(filter(df, is.na(year_opened_int))[,c("year_opened_str", "year_opened_int")])
 
-df$countrycode <- recode(df$country, "United Kingdom" = "GBR", "Spain" = "ESP", "United States" = "USA", "Switzerland" = "CHE" , "India" = "IND", "Greece" = "GRC", "Lebanon" = "LBN", "France" = "FRA", "Estonia" = "EST", "Azerbaijan" = "AZE", "Latvia" = "LVA", "Madagascar" = "MDG", "Indonesia" = "IDN", "Slovakia" = "SVK", "Romania" = "ROU","Argentina" = "ARG","South Korea" = "KOR", "Japan" = "JPN", "Benin" = "BEN", "Bangladesh" = "BGD", "Australia" = "AUS", "Norway" = "NOR", "New Zealand" = "NZL", "Poland" = "POL", "Nigeria" = "NGA", "Portugal" = "PRT", "Serbia" = "SRB","Czech Republic" = "CZE","Senegal" = "SEN", "Puerto Rico" = "PRI", "Taiwan" = "TWN", "Israel" = "ISR", "England" = "GBR", "China" = "CHN", "Germany" = "DEU", "Netherlands" = "NLD", "Italy" = "ITA", "Russia" = "RUS", "Canada" = "CAN", "Hungary" = "HUN", "Belgium" = "BEL", "Sweden" = "SWE", "Finland" = "FIN","Malaysia" = "MYS","Philippines" = "PHL", "Turkey" = "TUR", "Austria" = "AUT", "South Africa" = "ZAF","Thailand" = "THA", "Denmark" = "DNK",  "Mexico" = "MEX", "United Arab Emirates" = "ARE","Brazil" = "BRA", "Hong Kong" = "HKG", "Ukraine" = "UKR", "Kuwait" = "KWT",  "Cyprus" = "CYP", "Monaco" = "MCO", "Iceland" = "ISL", "Kenya" = "KEN", "Singapore" = "SGP", "Iran" = "IRN", "Lithuania" = "LTU", .default= NA_character_)
+    return(df)
+}
+
+dfx <- create_base_df()
+
+create_base_df_diagnose <- function(df, verbose = 0){
+    #' check status of base df
+    ## debugging unclear/missing countries: atm 12 cases
+
+    country_test <- filter(df, is.na(countrycode))[,c("country", "name")]
+    print(paste0("non-perfect countries: ", nrow(country_test)))
+    if (verbose == 1){
+        print(as.data.frame(country_test))
+    }
+
+    year_opened_test <- filter(df, is.na(year_opened_int))[,c("year_opened_str", "year_opened_int")]
+    print(paste0("non-perfect opening years: ", nrow(year_opened_test)))
+    if (verbose == 1){
+        print(as.data.frame(year_opened_test))
+    }
+    ## could functionalize this diagnosis properly with mapping variables to sort, with additional variables to be selected
+    ## but don't think I need it as this point, don't update data that much
+}
+
+create_base_df_diagnose(dfx)
+    
 
 ## ** set static vars
 FIG_DIR <- "/home/johannes/Dropbox/phd/papers/org_pop/figures/"
@@ -64,9 +97,6 @@ WID_DIR = "/home/johannes/Dropbox/phd/papers/org_pop/data/wid/"
 WID_FILES <- list.files(WID_DIR)
 
 con <- DBI::dbConnect(RClickhouse::clickhouse(), host="localhost", db = "org_pop")
-
-## ** custom funcs
-
 
 
 
@@ -264,16 +294,16 @@ filter(multi_inner, is.na(population))
 ## ** basic DV table preparation
 ## add year: 
 ## just clean everything by taking first 4, drop everything that doesn't work lmao
-df$year_opened_int <- as.integer(lapply(df$year_opened, function(x)(substring(x, 0,4))))
+
 ## have 59 NAs, oh well
 ## let's say for now 1985-2021
 # plot(table(df$year_opened_int), type='h')
 
 
-
 df$year_opened_int2 <- df$year_opened_int
 df$year_opened_int2[which(df$year_opened_int2 < STARTING_YEAR | df$year_opened_int2 > 2021)] <- NA
 ## up to 100 dropped atm 
+
 
 df_open <- na.omit(df[,c('name', 'country', 'countrycode', 'year_opened_int2')])
 # plot(table(df_open$year_opened_int2), type='l')
