3ab6e03309544ba457d6c1d863082e705a8b8985
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-02-10 10:46:00 +0100

    split of the WB api tests into separate file

---
+++ scripts/wb_api_playground.R
@@ -0,0 +1,62 @@
+## **** testing wbstats
+
+library(wbstats)
+
+x <- wb_indicators(lang="en")
+
+gdp_vars <- x[which(grepl("gdp", x$indicator, ignore.case = TRUE)),]
+gdp_vars$indicator
+gdp_vars$indicator_id
+
+filter(gdp_vars, indicator_id == "NY.GDP.MKTP.CD")$indicator_desc
+
+## try C1.12
+
+indicatorx <- "C1.12"
+indicatorx <- "C1.9"
+indicatorx <- "1.0.HCount.1.90usd"
+indicatorx <- "NY.GDP.MKTP.CD"
+
+z <- wb_data(indicator=indicatorx, start_date=2015, end_date=2000)
+y <- wb_data(indicator=indicatorx, mrv=10000)
+
+table(z$date)
+table(y$date)
+
+
+## **** getting gdp to work
+
+indicatorx <- "NY.GDP.PCAP.CD"
+
+gdp_api <- wb_data(indicator = indicatorx, start_date = STARTING_YEAR, end_date = ENDING_YEAR)
+
+df_gdp_pcap_molt$year <- as.numeric(as.character(df_gdp_pcap_molt$year))
+df_gdp_pcap_molt <- filter(df_gdp_pcap_molt, year > 1984)
+
+gdp_api_min <- gdp_api[,c("iso3c","country", "date", "NY.GDP.PCAP.CD")]
+names(df_gdp_pcap_molt) <- c("country", "iso3c", "date", "gdp_pcap")
+
+gdp_cpr <- as_tibble(merge(gdp_api_min, df_gdp_pcap_molt))
+gdp_cpr$diff <- gdp_cpr$NY.GDP.PCAP.CD - gdp_cpr$gdp_pcap
+
+table(filter(gdp_cpr, diff > 1000)$country)
+table(filter(gdp_cpr, diff > 100)$country)
+table(filter(gdp_cpr, diff > 1000)$date)
+
+as.data.frame(filter(gdp_cpr, diff > 1000))
+
+hist(gdp_cpr$diff, breaks = 1000)
+mean(gdp_cpr$diff, na.rm=T)
+
+nrow(filter(gdp_cpr, diff < 0.01))/nrow(filter(gdp_cpr, diff < 1e08))
+
+png(paste0(FIG_DIR, "myanmar_twitter.png"), width=800, height=600)
+filter(gdp_api, country %in% c("Myanmar", "United States", "Luxembourg")) %>%
+    ggplot(aes(x=date, y=NY.GDP.PCAP.CD, group=country, color = country)) +
+    geom_line(size=2) +
+    theme(legend.text = element_text(size=13))
+dev.off()
+
+filter(df_gdp_pcap_molt, country %in% c("Myanmar", "United States", "Germany", "Luxembourg")) %>%
+    ggplot(aes(x=date, y=gdp_pcap, group=country, color = country)) +
+    geom_line()
