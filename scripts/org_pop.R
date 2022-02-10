88913218e8b8b310d0c48725f1ef4e3e8f9310f4
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-02-10 10:40:05 +0100

    finish world bank API calls, add notes of it to todolist

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -165,53 +165,70 @@ filter(df_gdp_pcap_molt, country %in% c("Myanmar", "United States", "Germany", "
 
 download_WB_data <- function(indx){
     #' downloads data with the WB API, can take single WB variable codes or vectors thereof
+    print(paste0("now downloading", indx))
     indx_data <- wb_data(indicator = indx, start_date = STARTING_YEAR, end_date = ENDING_YEAR)
     return(indx_data[,c("iso3c", "country", "date", indx)])
 }
 
-dfx <- download_WB_data(c("NY.GDP.PCAP.CD", "SI.POV.GINI"))
+## dfx <- download_WB_data(c("NY.GDP.PCAP.CD", "SI.POV.GINI"))
 
 save_WB_data <- function(df) {
     #' saves WB data
     write.table(df, file = paste0(PROC_DATA_DIR, "WB_data_proc.csv"))
 }
 
-save_WB_data(dfx)
+## save_WB_data(dfx)
 
 
 read_WB_data <- function() {
     df_wb <- as_tibble(read.table(paste0(PROC_DATA_DIR, "WB_data_proc.csv")))
 }
 
-df_wb <- read_WB_data()
+## df_wb <- read_WB_data()
+
+
 
 get_WB_data <- function(indx, refresh_all=FALSE) {
     #' get WB variables, download all new if asked to do so, else read from file
     df_wb_local <- read_WB_data()
     wb_vars_there <- names(df_wb_local)[4:len(names(df_wb_local))]
-
+    print(paste0("wb_vars_there: ", wb_vars_there))
 
     if (refresh_all) {
-        print(indx)
+        print(paste0("download all indicators anew: ", indx))
         df_wb_api <- download_WB_data(indx)
 
         unchanged_vars <- wb_vars_there[which(wb_vars_there %!in% indx)]
         
     } else {
         wb_vars_to_download <- indx[which(indx %!in% wb_vars_there)]
-        print(wb_vars_to_download)
+        print(length(wb_vars_to_download))
+        print(paste0("download selected indicator(s) anew: ", wb_vars_to_download)) #
+        
+        if (length(wb_vars_to_download) > 0) {
+            df_wb_api <- download_WB_data(wb_vars_to_download)
+        } else {
+            df_wb_api <- df_wb_local[,c("iso3c", "country", "date")]
+        }
 
-        unchanged_vars <- wb_vars_there[which(wb_vars_to_download %!in% wb_vars_to_download)]
-        print(unchanged_vars)
-        ## need to this out: how to get the variables which shouldn't be changed
+        unchanged_vars <- wb_vars_there[which(wb_vars_there %!in% wb_vars_to_download)]
+        print(paste0("unchanged_vars: ", unchanged_vars))
+        ## need to this out: how to get the variables which shouldn't be changed; think i got it now
     }
+
+    df_wb_new <- as_tibble(merge(df_wb_local[,c("iso3c", "country", "date", unchanged_vars)], df_wb_api))
+    save_WB_data(df_wb_new)
+
+    return(df_wb_new)
     
 }
 
 
 indx <- c("NY.GDP.PCAP.CD", "SI.POV.GINI", "NY.GDP.PCAP.KD.ZG")
-get_WB_data(indx, FALSE)
-x <- TRUE
+x <- get_WB_data(indx, TRUE)
+
+
+
 
 
 
