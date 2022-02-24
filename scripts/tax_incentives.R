4d84f1bae6c05ac9647f66545222daf40dbafbac
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-02-24 12:30:57 +0100

    get distinctive colors from https://medialab.github.io/iwanthue/

--- scripts/tax_incentives.R
+++ scripts/tax_incentives.R
@@ -1,3 +1,10 @@
+colors_manual <- c("#c78ab5","#74b648","#6f4aca","#c7994f","#cb4fc2","#516833","#cd4872","#5cb099","#d25133","#6289c0","#814135","#613a76")
+
+colors_manual <- c("#01867b","#e2183d","#44ddb8","#891ba6","#93d93d","#ff3c8a","#006e00","#d2b1ff","#ff9e30","#009cf4","#ff6830","#86375a","#ae8400","#967649")
+
+colors_manual_light <- c("#a4e3a5","#f197c1","#98fff4","#f29a83","#2bcef0","#ffefa5","#75bfff","#c2bc71","#d6d3ff","#52bcae","#ffc9c2","#85b4b6","#f3ffd5","#b0ad84")
+
+
 
 ## * reading in 
 TAX_INCENTIVES_DIR <- paste0(PROJECT_DIR, "data/tax_incentives/")
