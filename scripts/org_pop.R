af269715d3d6c9c247ccb7ed3e1ade441251a1c4
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-02-28 12:55:25 +0100

    construct MOW extraction functions for cross-sectional/longitudinal contemp/all art/all museum founding counts

--- scripts/org_pop.R
+++ scripts/org_pop.R
@@ -647,58 +647,10 @@ some_page <- x[[1]]
 
 ## first do in python: mow.py
 
-
-## https://stackoverflow.com/questions/48350991/filter-a-dataframe-by-values-in-a-column-of-type-list
-
-## ** json 
-
-library('tidyverse')
-
-df <- tribble(
-  ~rownum, ~categories,
-        1, 'b',
-        2, c('c', 'd'),
-        3, c('d', 'e')
-)
-
-# All rows containing the 'd' category
-df %>%
-    filter(map_lgl(categories, ~'d' %in% .)) %>%
-  str
-
-
-
-write.csv(df, paste0(PROC_DATA_DIR, "listdf.csv"))
-library(readr)
-write_lines(df, paste0(PROC_DATA_DIR, "listdf.csv"))
-read_lines(paste0(PROC_DATA_DIR, "listdf.csv"))
-read.csv(paste0(PROC_DATA_DIR, "listdf.csv"))
-
-filter(df, map_lgl(categories, ~'d' %in% .))
-
-## ** json
-library(rjson)
-json_res <- fromJSON(file = paste0(PROC_DATA_DIR, "mow.json"))
-
-list_cols <- c("type", "clsfcn")
-
-for (i in (setdiff(names(json_df), list_cols))) {
-    json_df[,i] <- unname(unlist(json_df[,i]))
-}
-
-json_res <- fromJSON(file = paste0(PROC_DATA_DIR, "test.json"))
-
-json_df <- as_tibble(as_data_frame(json_res))
-
-
-json_df$ctr <- 1
-json_df$founding_date1 <- as.numeric(json_df$founding_date1)
-
-ggplot(aggregate(ctr ~ founding_date1, filter(json_df, map_lgl(type, ~'Art Museum' %in% .) & founding_date1 > 1900), sum), aes(x=founding_date1, y=ctr)) +
-    geom_line()
-    
-
 ## ** csv
+
+
+
 df_mow <- as_tibble(read.csv(paste0(MOW_DIR, "mow.csv")))
 
 df_mow$iso3c <- countrycode(df_mow$country, "country.name", "iso3c")
@@ -706,30 +658,121 @@ df_mow$iso3c <- countrycode(df_mow$country, "country.name", "iso3c")
 mow_type <- as_tibble(read.csv(paste0(MOW_DIR, "type.csv")))
 mow_clsfcn <- as_tibble(read.csv(paste0(MOW_DIR, "classification.csv")))
 
-mow_fndgs <- as_tibble(merge(filter(df_mow[,c("idx", "name", "founding_date1", "country")], founding_date1 > 1900), mow_type, by=c("idx")))
+mow_fndgs <- as_tibble(merge(filter(df_mow[,c("idx", "name", "founding_date1", "iso3c")], founding_date1 > 1900), mow_type, by=c("idx")))
 
 mow_fndgs$cnt <- 1
-
-mow_fndgs_grp <- as_tibble(aggregate(cnt ~ founding_date1 + type, mow_fndgs, sum))
-
-library(tidyquant)
-
-max_grps <- aggregate(cnt ~ type, mow_fndgs, sum)
-max_grps <- max_grps[rev(order(max_grps$cnt))[1:12],"type"]
-
-pdf(paste0(FIG_DIR, "mow.pdf"), height = 5, width = 10)
-
-ggplot(filter(mow_fndgs_grp, type %in% max_grps), aes(x=founding_date1, y=cnt, group=type, color = type)) +
-    geom_ma(ma_fun = SMA,n=8, size=1, linetype="solid") +
-    scale_color_brewer(palette="Paired") +
-    labs(y="nbr openings", x="founding date")
-
-dev.off()
 
 viz_lines(mow_fndgs, x="founding_date1", y="cnt", time_level = "ra", duration = 8, grp = "type", extra = FALSE)
 
 
 
+
+
+## check that viz_lines can also be used to aggregate dataframe
+y <- viz_lines(mow_fndgs, x="founding_date1", y="cnt", time_level = "ra", duration = 1, grp = "iso3c", extra = FALSE, fill_up = T)
+
+y <- viz_lines(na.omit(mow_fndgs[,c("founding_date1", "cnt", "iso3c")]), x="founding_date1", y="cnt", time_level = "ra", duration = 1, grp = "iso3c", extra = FALSE, fill_up = T)
+
+
+y2 <- fill_up2(na.omit(mow_fndgs[,c("founding_date1", "cnt", "iso3c")]), x="founding_date1", y="cnt", grp="iso3c")
+
+y2 <- fill_up2(mow_fndgs, x="founding_date1", y="cnt", grp="iso3c")
+
+head(filter(y, grp == "DEU"))
+
+filter(mow_fndgs, is.na(iso3c))
+       
+
+viz_lines(filter(mow_fndgs, founding_date1 > 1985), x="founding_date1", y="cnt", time_level = "ra", duration = 3, grp = "iso3c", fill_up = T)
+
+x <- as_tibble(aggregate(cnt ~ founding_date1 + iso3c, mow_fndgs, sum))
+head(filter(x, iso3c == "DEU"),8)
+
+
+
+mow_tag_year_cnt <- function(mow_df, year, setx, sets) {
+    #' get the counts of mow df until year given some set name
+    #' don't pass list of tags directly because it's a mess to then get the label 
+
+    tags <- sets[setx]
+
+    print(paste0(year, setx))
+
+    mow_df_fltrd <- filter(mow_df, founding_date1 <= year, tag %in% tags[[1]])
+    mow_df_fltrd2 <- unique(mow_df_fltrd[,c('idx', 'name', 'iso3c')])
+    mow_df_fltrd2$cnt <- 1
+                                
+
+    mow_agg <- as_tibble(aggregate(cnt ~ iso3c, mow_df_fltrd2, sum))
+
+
+    names(mow_agg)[2] <- paste0('cnt_', setx, "_", year)
+
+    return(mow_agg)
+    
+}
+
+mow_cntns_cvrg <- function(mow_df, setx, sets) {
+    #' construct the continuous measures (just to test them)
+
+    tags <- sets[setx]
+
+    mow_df_fltrd <- filter(mow_df, founding_date1 >= STARTING_YEAR & tag %in% tags[[1]])
+    mow_df_fltrd2 <- unique(mow_df_fltrd[,c("idx", "name", "iso3c", "founding_date1")])
+    mow_df_fltrd2$cnt <- 1
+
+    mow_agg <- as_tibble(aggregate(cnt ~ iso3c + founding_date1, mow_df_fltrd2, sum))
+    
+    names(mow_agg) <- c("iso3c", "year", paste0("cnt_", setx))
+    return(mow_agg)
+
+}
+
+
+
+get_mow_dfs <- function() {
+    #' provide the MOW data
+    
+    df_mow <- as_tibble(read.csv(paste0(MOW_DIR, "mow.csv")))
+    df_mow$iso3c <- countrycode(df_mow$country, "country.name", "iso3c")
+
+    mow_type <- as_tibble(read.csv(paste0(MOW_DIR, "type.csv")))
+    mow_clsfcn <- as_tibble(read.csv(paste0(MOW_DIR, "classification.csv")))
+    names(mow_type) <- c("idx", "tag")
+    names(mow_clsfcn) <- c("idx", "tag")
+
+    mow_tag <- rbind(mow_type, mow_clsfcn)
+
+    mow_tag <- as_tibble(merge(filter(df_mow[,c("idx", "name", "founding_date1", "iso3c")], founding_date1 > 1900), mow_tag, by=c("idx"), all=T))
+
+    
+    set_contemp <- c("Art, Modern and Contemporary")
+    set_art <- c("Art Museum", "Art")
+    ## set_all <-list("asdf", "jjj")
+    set_all <- setdiff(unique(mow_tag$tag), c(set_contemp, set_art))
+    
+    
+    sets <- list("contemp"=set_contemp, "art"=set_art, "all"=set_all)
+
+    years <- c(1985, 2000,2020)
+    set_year_cbns <- as_tibble(expand.grid(set=names(sets), year=years))
+    
+    res_mow_agg <- apply(set_year_cbns, 1, function(x) mow_tag_year_cnt(mow_df=mow_tag, year=x['year'], setx=x['set'], sets=sets))
+    ## mow_tag_year_cnt(mow_tag, year=2000, setx="art")
+
+    mow_res_wide <- as_tibble(Reduce(function(x,y,...) merge(x,y,by=c('iso3c'), all=TRUE), res_mow_agg))
+    mow_res_wide[is.na(mow_res_wide)] <- 0
+
+
+    ## res_mow_cnts <- mow_cntns_cvrg(mow_tag, setx="art")
+    res_mow_cnts <- lapply(names(sets), function(x) mow_cntns_cvrg(mow_df = mow_tag, setx = x, sets=sets))
+    res_mow_cnts_cbn <- as_tibble(Reduce(function(x,y,...) merge(x,y,by=c('iso3c', 'year'), all=TRUE), res_mow_cnts))
+
+ 
+    return(list(
+        mow_crssctn=mow_res_wide,
+        mow_cntns = res_mow_cnts_cbn))
+}
 
 ## ** more cleaning/completeness checks
 ## *** country
