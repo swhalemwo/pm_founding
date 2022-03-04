368e27ed6e176d861ade908b2b5aa12b8b171745
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-04 16:19:12 +0100

    add pseudo-r2, overdispersion phi, try interaction, descriptives, non-integers (doesn't work)

--- scripts/poisson_test.R
+++ scripts/poisson_test.R
@@ -1,13 +1,53 @@
 library(tidyverse)
-library(screenreg)
+library(texreg)
+library(MASS)
 
 df <- as_tibble(
     read.csv("/home/johannes/Dropbox/phd/papers/org_pop/data/poisson_test_data/Coxe et al. Poisson Dataset in Text Format.txt", header=FALSE, sep= " "))
 names(df) <- c("id", "sensation", "gender", "drinks")
 
+df$drinks_mod <- df$drinks + 0.01
+
 ols <- lm(drinks ~ sensation, df)
 poi1 <- glm(drinks ~ sensation, df, family = "poisson")
 poi2 <- glm(drinks ~ sensation + gender, df, family = "poisson")
+poi_interact <- glm(drinks ~ sensation*gender, df, family = "poisson")
+nb1 <- glm.nb(drinks ~ sensation, df)
 
-screenreg(list(ols, poi1, poi2, ))
 
+screenreg(list(ols, poi1, poi2, nb1, poi_interact))
+
+resids <- residuals(poi2, "response")
+resids <- residuals(poi2, "deviance")
+
+plot(poi2$fitted.values, resids)
+
+
+
+
+
+pseudo.r2 <- function(glm) {
+    #' pseudo r-square following Coxe_West_Aiken_2009_count
+    return(1-(glm$deviance/glm$null.deviance))
+}
+
+poisson.phi <- function(glm) {
+    #' calculate overdispersion parameter phi
+    dp = sum(residuals(glm,type ="pearson")^2)/glm$df.residual
+    return(dp)
+}
+
+
+## ** descriptives 
+
+aggregate(drinks ~ gender, df, mean)
+plot(df$sensation, df$drinks)
+
+ggplot(df, aes(x=sensation, y=drinks, color=factor(gender))) +
+    geom_jitter(width=0.5, height=0.5)
+
+
+
+## **  test how R deals with non-integer things
+poi_mod <- glm(drinks_mod ~ sensation + gender, df, family = "poisson")
+## coefs still get estimated, fit stats (AIC, BIC, LL) are gonesies
