7b400982abde3cbe1f76fe4e225ee52b0edda9ab
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-07 11:56:13 +0100

    test poisson fe with glmer, so far unclear

--- scripts/poisson_test.R
+++ scripts/poisson_test.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(texreg)
 library(MASS)
+library(lme4)
 
 df <- as_tibble(
     read.csv("/home/johannes/Dropbox/phd/papers/org_pop/data/poisson_test_data/Coxe et al. Poisson Dataset in Text Format.txt", header=FALSE, sep= " "))
@@ -92,7 +93,7 @@ summary(m)
 plot(m) ## buggy 
 
 ## hope it also works for all the glmer.nb models...
-
+## seems to be the case 
 
 
 
@@ -150,6 +151,66 @@ ggplot(df, aes(x=sensation, y=drinks, color=factor(gender))) +
 
 
 
-## **  test how R deals with non-integer things
+## ** test how R deals with non-integer things
 poi_mod <- glm(drinks_mod ~ sensation + gender, df, family = "poisson")
 ## coefs still get estimated, fit stats (AIC, BIC, LL) are gonesies
+
+## ** testing FE
+
+poi_fe_df <- as_tibble(cbind(c(1,1,1,2,2,2,3,3,3),
+                             c(1,2,3,4,5,6,7,8,9)))
+names(poi_fe_df) <- c("id", "y")
+## poi_fe_df$x_within_across <- poi_fe_df$y + rnorm(poi_fe_df$y, sd = 0.5)
+poi_fe_df$x_within_across <- poi_fe_df$y
+poi_fe_df$x_within <- rep(c(1,2,3), 3)
+poi_fe_df$x_across <- sort(poi_fe_df$x_within)
+poi_fe_df$size <- poi_fe_df$id*1000
+poi_fe_df$x_within2 <- c(-2, -1, 0, 8,9,10,4,5,6)
+
+
+ols_base <- lm(y ~ x_within_across, poi_fe_df)
+poi_base <- glm(y ~ x_within_across, poi_fe_df, family = "poisson")
+summary(poi_base)
+screenreg(list(ols_base, poi_base))
+pseudo.r2(poi_base)
+
+poi_fe_within_across <- glmer(y ~ x_within_across + (1 | id), poi_fe_df, family = "poisson")
+poi_fe_within <- glmer(y ~ x_within + (1 | id), poi_fe_df, family = "poisson")
+poi_fe_across <- glmer(y ~ x_across + (1 | id), poi_fe_df, family = "poisson")
+
+screenreg(list(poi_base, poi_fe_within_across, poi_fe_within, poi_fe_across))
+## kinda worrying that poi_fe_within doesn't find the within relationship
+## maybe it's only demeaning the predictors? 
+
+## testing more ways of getting fe to work
+
+poi_fe_within2 <- glmer(y ~ x_within2 + (1 | id), poi_fe_df, family = "poisson")
+poi_fe_within3 <- glmer(x_within ~ x_within2 + (1 | id), poi_fe_df, family = "poisson")
+poi_fe_within4 <- glm(x_within ~ x_within2 + factor(id), poi_fe_df, family = "poisson")
+
+poi_fe_df <- poi_fe_df %>%
+    group_by(id) %>%
+    mutate(x_within3 = x_within2 - mean(x_within2))
+poi_fe_within5 <- glm(x_within ~ x_within3, poi_fe_df, family = "poisson")
+
+poi_fe_df$x_within4 <- poi_fe_df$x_within3 + 2
+poi_fe_within6 <- glm(x_within ~ x_within4, poi_fe_df, family = "poisson")
+
+screenreg(list(poi_fe_within, poi_fe_within2, poi_fe_within3, poi_fe_within4, poi_fe_within5, poi_fe_within6))
+## hmm not good, x_within 2 should predit x_within after being demeaned
+## still not good: controlling for factor(id) should make x_within2 significant
+
+## hmm even predicting itself is not significant?? at least with4/5/6 always give the same result of 0.52 for demeaning, factor and identical
+## -> challenge is now to get the same from glmer
+
+## screenreg(glm(y ~ x_within_across, poi_fe_df, family = "poisson"))
+## screenreg(glm(x_within ~ x_within4, poi_fe_df, family = "poisson"))
+
+
+
+
+poi_fe_within21 <- glmer(y ~ x_within3 + (1 | id), poi_fe_df, family = "poisson")
+poi_fe_within22 <- glmer(y ~ x_within4 + (1 | id), poi_fe_df, family = "poisson")
+
+
+screenreg(list(poi_fe_within2, poi_fe_within21, poi_fe_within22))
