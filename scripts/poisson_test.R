5f44afb50efd2c292e911f9a9d829651d8a8d55f
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-07 10:43:54 +0100

    prediction plotting, test interaction interpretation, add AME tests

--- scripts/poisson_test.R
+++ scripts/poisson_test.R
@@ -28,9 +28,9 @@ plot(poi2$fitted.values, resids)
 predict(poi2)
 predict_df <- as_tibble(expand.grid(gender=c(0,1), sensation=seq(0,8,0.1)))
 
-predict_df$prediction <- predict.glm(poi2, newdata=predict_input, type="response")
+predict_df$prediction <- predict.glm(poi_interact, newdata=predict_df, type="response")
 
-predict_se <- predict.glm(poi2, newdata=predict_input, type="response", se.fit=T)
+predict_se <- predict.glm(poi_interact, newdata=predict_df, type="response", se.fit=T)
 predict_df$prediction <- predict_se$fit
 predict_df$se <- predict_se$se.fit
 ## options for type:
@@ -48,6 +48,51 @@ ggplot(predict_df, aes(x=sensation, y=prediction, color=factor(gender))) +
 ## makes sense that men start higher: exp(0) for gender evaluates to 1, so if men have something more there (0.83) they have more because there are is still non-zero baseline to multiplicate with 
 exp(-0.78) ## women at sensation 0
 exp(-0.78) * exp(0.83) ## men at sensation 0
+
+## huh the interaction is strong
+##
+## (Intercept)       0.05182
+## sensation         0.10318: slope of sensation when gender ==0
+## gender           -0.30196: slope of gender when senation==0
+## sensation:gender  0.21434
+
+## men at 0 sensation:
+filter(predict_df, gender==1 & sensation == 0)
+
+exp(0.05181 + 0.103*0 + -0.3 + 0.21) ## nope 
+exp(0.05181 + -0.30196) ## yup: only gender coef now matters because sensation is 0
+
+## men at sensation 5
+filter(predict_df, gender==1 & sensation == 5)
+exp(0.05181 + 0.21434*5) ## nope
+exp(0.05181 + 0.10318 * 5 + -0.30196 + 0.21434*5) ## brainlet
+exp(0.05181 + (0.10318 + 0.21434)*5 + (-0.30196 + 0.21434)*1)
+exp(0.05181 + (0.10318 + 0.21434)*5 + (-0.30196 + 0.21434)*1)
+
+as.data.frame(filter(predict_df, gender==1 & sensation == 7.4))
+
+exp(0.05181 + 0.10318 *7.4  + (-0.30196) + 0.21434*7.4) ## this makes no sense
+## why is interaction coef added overall and not to each? 
+exp(0.05181 + (0.103181 + 0.21434) * 7.4 + (-0.30196))
+## now interaction coef only added to sensation coef
+## hm that's cause gender coef is 1
+## -> so I can read it as normal linear adding up coefs for prediction purposes
+
+
+## women at sensation 5
+filter(predict_df, gender==0 & sensation == 5)
+exp(0.05181 + 0.103*5)
+
+
+
+## *** AME
+library(margins)
+m <- margins(poi_interact, type="link")
+summary(m)
+plot(m) ## buggy 
+
+## hope it also works for all the glmer.nb models...
+
 
 
 
