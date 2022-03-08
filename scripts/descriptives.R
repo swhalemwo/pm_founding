4a971423e98d5c733942935f9f2f73f343612263
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-08 13:52:00 +0100

    add PM opening counts/rates figure, tax incentive descriptives, text on both DV and tax incentives

--- scripts/descriptives.R
+++ scripts/descriptives.R
@@ -1,3 +1,23 @@
 
 ## generate the descriptives tables/figures for the update
 
+df_taxinc <- read_in_tax_incentives()
+library(psych)
+
+tax_inc_descriptives <- describe(df_taxinc[tax_vars_caf], skew = F)
+
+tax_inc_xtbl <- xtable(
+    tax_inc_descriptives[names(tax_inc_descriptives) != "vars"],
+    label = "tax_inc_descriptives",
+    caption = "Tax Incentive Descriptives"
+    )
+
+print(tax_inc_xtbl,
+      include.rownames = T,
+      file = paste0(TABLE_DIR, "tax_inc_descriptives.tex"),
+      ## tabular.environment = 'longtable'
+      )
+
+
+
+
