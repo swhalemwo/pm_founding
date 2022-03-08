580cc1ce633c15cea9ff915a2ad0446c2d1118b4
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-08 15:54:32 +0100

    add plots, tables and text for tax incentive results

--- scripts/descriptives.R
+++ scripts/descriptives.R
@@ -14,10 +14,20 @@ tax_inc_xtbl <- xtable(
 
 print(tax_inc_xtbl,
       include.rownames = T,
-      file = paste0(TABLE_DIR, "tax_inc_descriptives.tex"),
-      ## tabular.environment = 'longtable'
+      floating = FALSE,
+      file = paste0(TABLE_DIR, "tax_inc_descriptives.tex")
       )
 
+res.pca.caf <- prcomp(na.omit(df_taxinc[,tax_vars_caf]),scale = T)
+df_taxinc$region <- countrycode(df_taxinc$iso3c, "iso3c", "un.regionsub.name")
 
+p.scree <- fviz_eig(res.pca.caf, title = "scree plot")
+p.arrows <- viz_pca_arrows(res.pca.caf, title = "factor loadings")
 
+pdf(paste0(FIG_DIR, "tax_inc_pca_caf.pdf"), height = 5, width = 10)
+grid.arrange(grobs = list(p.scree, p.arrows), ncol=2, as.table=F)
+dev.off()
 
+pdf(paste0(FIG_DIR, "tax_inc_pca_caf_ind.pdf"), height = 11, width = 19)
+create_ind_plot(df_taxinc, res.pca.caf, label_col = "country", title = "CAF country plot", color_col = "region")
+dev.off()
