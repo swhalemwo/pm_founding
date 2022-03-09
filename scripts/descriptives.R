8beb558a64bef145c717d99a2cf435d589f80d04
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-09 10:30:45 +0100

    mow descriptives generation and text

--- scripts/descriptives.R
+++ scripts/descriptives.R
@@ -1,10 +1,13 @@
+    library(psych)
 
-## generate the descriptives tables/figures for the update
+## * generate the descriptives tables/figures for the update
+
+### ** tax incentives
 
 generate_taxinc_descriptives <- function() {
 
     df_taxinc <- read_in_tax_incentives()
-    library(psych)
+
 
     tax_inc_descriptives <- describe(df_taxinc[tax_vars_caf], skew = F)
 
@@ -39,3 +42,64 @@ generate_taxinc_descriptives <- function() {
 }
 
 generate_taxinc_descriptives() 
+
+
+## ** mow
+
+get_mow_descriptives <- function() {
+    
+    mow_res <- get_mow_dfs()
+
+    mow_df <- mow_res$mow_crssctn
+
+    
+    mow_cntns <- mow_res$mow_cntns
+
+    cry_df <- as_tibble(unique(df_anls$iso3c), .name_repair = ~c("iso3c"))
+    mow_df <- as_tibble(merge(cry_df, mow_df, all.x = T))
+
+    mow_df[is.na(mow_df)] <- 0
+    
+    
+    mow_descs <- describe(mow_df[names(mow_df) != "iso3c"], skew = F)
+    mow_tbl <- xtable(mow_descs[names(mow_descs) %!in% c("vars", "se")],
+                      label = "mow_descriptives",
+                      caption = "Museum of the World Descriptives")
+
+    print(mow_tbl, include.rownames=T, file = paste0(TABLE_DIR, "mow_descriptives.tex"))
+
+
+    ## also add the longitudinal measures here for the argument that in recent years coverage has declined
+    ## also add that many recently opened private museums are not included, such as Voorlinden, Inhotim, Long Museum
+
+
+    ## maybe facets
+    expand_df <- tidyr::expand(mow_cntns, year=year, iso3c=iso3c)
+    mow_lines <- as_tibble(merge(expand_df, mow_cntns, all.x = T))
+    mow_lines[is.na(mow_lines)] <- 0
+
+    mow_lines_melt <- as_tibble(reshape2::melt(mow_lines, id=c("year", "iso3c")))
+
+    max_crys <- aggregate(value ~ iso3c, mow_lines_melt, sum) %>%
+        arrange(value) %>%
+        top_n(8) %>%
+        pull(iso3c) 
+    
+
+    library(tidyquant)
+    
+    mow_facets <- ggplot(filter(mow_lines_melt, iso3c %in% max_crys), aes(x=year, y=value, color = iso3c)) +
+        facet_wrap(~ variable, scales = "free") + 
+        geom_ma(n=5, linetype="solid") +
+        scale_color_brewer(palette = "Paired") +
+        labs(y="count of museum openings, 5 year rolling avg")
+
+    
+    pdf(paste0(FIG_DIR, "mow_facets.pdf"), width = 8, height = 4)
+    plot(mow_facets)
+    dev.off()
+    
+}
+
+get_mow_descriptives()
+    
