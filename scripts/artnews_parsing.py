4cfdc78f0fc4cc7d0d30987261d1fb3a9ae2c10f
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-03-30 18:41:45 +0200

    functionalize information extraction, more comfy with dicts

--- scripts/artnews_parsing.py
+++ scripts/artnews_parsing.py
@@ -33,17 +33,40 @@ def extract_cltr_info(nbr, ranking_soup):
 
 
     # clctr_name = re.findall('<img alt="(.*?)"', str(prfl))
-    clctr_name = re.findall("\t\t\t\t\t(.*?)\t\t\n\t", str(prfl))
-    location = re.findall('u-color-red lrv-u-padding-tb-050">(.*?)</p>', str(prfl))
-    collection_focus = re.findall('u-background-color-red:before a-icon-float-left">(.*?)</p>', str(prfl), re.S)
-    industry = re.findall('lrv-u-font-family-primary lrv-u-display-block">(.*?)</p>', str(prfl))
+    regex_dict = {
+    'clctr_name' :"\t\t\t\t\t(.*?)\t\t\n\t",
+    'location' :'u-color-red lrv-u-padding-tb-050">(.*?)</p>',
+    'collection_focus' : 'u-background-color-red:before a-icon-float-left">(.*?)</p>',
+    'industry' : 'lrv-u-font-family-primary lrv-u-display-block">(.*?)</p>'}
 
-    return({
-        'clrct_name': clctr_name,
-        'location': location,
-        'collection_focus': collection_focus,
-        'industry': industry
-    })
+    
+    # clctr_name = re.findall("\t\t\t\t\t(.*?)\t\t\n\t", str(prfl))
+    # location = re.findall('u-color-red lrv-u-padding-tb-050">(.*?)</p>', str(prfl))
+    # collection_focus = re.findall('u-background-color-red:before a-icon-float-left">(.*?)</p>', str(prfl), re.S)
+    # industry = re.findall('lrv-u-font-family-primary lrv-u-display-block">(.*?)</p>', str(prfl))
+    data_dict = {k:extract_info(prfl, v) for k,v in regex_dict.items()}
+
+    return(data_dict)
+
+
+def extract_info(prfl, regex):
+
+    return_obj = []
+
+    res = re.findall(regex, str(prfl), re.S)
+    if res != []:
+        return_obj = res[0]
+        return_obj = re.sub(r"[\n\t]*", "", return_obj)
+
+    return(return_obj)
+
+
+
+# extract_info(prfl, "\t\t\t\t\t(.*?)\t\t\n\t")
+
+    
+# {k:extract_info(prfl, v) for k,v in regex_dict.items()}
+
 
 def proc_year(year):
     ranking_soup = get_year_ranking(year)
