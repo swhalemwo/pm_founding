d971c6a047553a3988d225cc0fce0dc1ef8d9ab2
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-04-13 09:44:16 +0200

    functionalize reading in, cpltns checks, start integrating website genre information, functionalize uniqueness checks

--- scripts/artnews.R
+++ scripts/artnews.R
@@ -2,31 +2,156 @@
 
 ARTNEWS_DIR <- paste0(PROJECT_DIR, "data/artnews/")
 
-artnews_df <- as_tibble(read.csv(paste0(ARTNEWS_DIR, "ranking.csv")))
+readin_artnews <- function() {
+    #' just read in the artnews df, fix the country coding
+    
+    artnews_df <- as_tibble(read.csv(paste0(ARTNEWS_DIR, "ranking.csv")))
 
-https://www.artnews.com/art-collectors/top-200-profiles/?filter_collectingarea=contemporary-art&year=1990
 
-https://www.artnews.com/art-collectors/top-200-profiles/?filter_top200year=1995&filter_collectingarea=contemporary-art&page=1
+    unique(artnews_df$location)
 
-unique(artnews_df$location)
+    ## can use countrycode
+    ## multiple matches return NA -> will be manually coded
+    artnews_df$country1 <- countrycode(artnews_df$location, "country.name", "iso3c")
 
-## can use countrycode
-## multiple matches return NA -> will be manually coded
-artnews_df$country1 <- countrycode(artnews_df$location, "country.name", "iso3c")
+    ## manually countrycoding the rest like a fucking LOSER
+    artnews_country_dict <- as_tibble(read.csv(paste0(ARTNEWS_DIR, "artnews_loc_table.csv")))
 
-## manually countrycoding coding the rest like a fucking LOSER
-artnews_country_dict <- as_tibble(read.csv(paste0(ARTNEWS_DIR, "artnews_loc_table.csv")))
+    ## reading the manually coded countrycodes in
+    artnews_cbn <- as_tibble(merge(artnews_df, artnews_country_dict, all.x = T))
 
-artnews_cbn <- as_tibble(merge(artnews_df, artnews_country_dict, all.x = T))
+    artnews_cbn$country3 <- ifelse(!is.na(artnews_cbn$country1), artnews_cbn$country1, artnews_cbn$country2)
 
-## filter(artnews_cbn, is.na(country1) & is.na(country2))$location
-      
+    ## filter(artnews_cbn, is.na(country1) & is.na(country2))$location
+    ## 14 people have no location
+    ## filter(artnews_cbn, clctr_name == "[]")
+    ## 5 have no name, could check them out since I have their position
+    ## filter(artnews_cbn, collection_focus == "[]")
+    ## 207 have no collection focus
 
-artnews_cbn$country3 <- ifelse(!is.na(artnews_cbn$country1), artnews_cbn$country1, artnews_cbn$country2)
 
-artnews_sep <- separate_rows(artnews_cbn, country3, sep=";")
-table(artnews_sep$country3) %>% sort()
-countrycode(unique(artnews_sep$country3), "iso3c", "country.name")    
+    artnews_sep <- separate_rows(artnews_cbn, country3, sep=";")
+    ## table(artnews_sep$country3) %>% sort()
+    ## countrycode(unique(artnews_sep$country3), "iso3c", "country.name")    
+
+    return (select(artnews_sep, location, clctr_name, collection_focus, industry, year, country=country3))
+}
+
+artnews_sep <- readin_artnews()
+## 14 
+
+readin_artnews_genre <- function() {
+    #' read in genre-classified artnews ranking
+    ##:ess-bp-start::browser@nil:##
+browser(expr=is.null(.ESSBP.[["@2@"]]));##:ess-bp-end:##
+    
+    1
+    
+
+    artnews_df <- as_tibble(read.csv(paste0(ARTNEWS_DIR, "ranking_genre.csv")))
+
+    ## artnews_df %>%
+    ##     group_by(year) %>%
+    ##     summarize(clctr_nbr = len(unique(clctr_name))) %>%
+    ##     as.data.frame() %>%
+    ##     plot(type='l')
+
+    select(artnews_df, clctr_name, year, collection_focus, genre)
+
+        
+}
+
+
+generate_unq_nbrs <- function(dfx, group, vlu) {
+    #' generates the df to check change over time 
+                              
+    dfx_unq_cntd <- dfx %>%
+        group_by_at(group) %>%
+        mutate(nbr_unq = n_distinct(get(vlu)))
+
+    return (dfx_unq_cntd)
+}
+
+change_checker <- function(dfx, vlu, group) {
+    #' checks whether values of vlu are identical for every entry of group
+    
+    dfx_unq_cntd <- generate_unq_nbrs(dfx, group, vlu)
+        
+    return_value <- FALSE
+
+    if (max(dfx_unq_cntd$nbr_unq)==1){
+        return_value <- TRUE
+    }
+
+    return(return_value)
+}
+
+
+get_changed_values <- function(dfx, vlu, group, verbose=F, return_value="info") {
+    #' report the non-unique values per group
+    #' also statistics would be nice
+    
+
+    dfx_unq_cntd <- generate_unq_nbrs(dfx, group, vlu)
+    
+    prop_calc_df <- dfx_unq_cntd %>%
+        group_by_at(group) %>%
+        summarize(group=sample(group,1), nbr_unq=sample(nbr_unq,1))
+    
+    prop_ones <- len(which(prop_calc_df$nbr_unq==1))/nrow(prop_calc_df)
+    cnt_non_ones <- len(which(prop_calc_df$nbr_unq != 1))
+
+    print(paste0(cnt_non_ones, " groups have different values, ", round(100* prop_ones,3),"% are identical"))
+
+    if (verbose) {
+
+        filter(dfx_unq_cntd, nbr_unq > 1) %>%
+            select_at(c(group,vlu)) %>%
+            unique() %>%
+            as.data.frame() %>%
+            print()
+    }
+
+    if (return_value == "info") {
+
+        return_obj <- list(
+            prop_ones = prop_ones,
+            cnt_non_ones = cnt_non_ones)
+    } else {
+        return_obj <- NULL
+    }
+    return(return_obj)
+
+}
+           
+
+
+change_checker(artnews_sep, vlu="collection_focus", group="clctr_name")
+change_checker(artnews_sep, vlu="collection_focus", group=c("clctr_name", "year"))
+
+get_changed_values(artnews_sep, vlu="collection_focus", group="clctr_name", verbose = T)
+
+## number of genres doesn't change for collector over year, content could still change 
+    d1 <- artnews_df %>%
+        group_by(clctr_name, year) %>%
+        summarize(nbr_genres_year = len(unique(genre))) %>%
+        group_by(clctr_name) %>%
+        summarize(nbr_genres_clctr_min = min(unique(nbr_genres_year)),
+                  nbr_genres_clctr_max = max(unique(nbr_genres_year))
+                  )
+    ## max and min are the same
+    d1$nbr_genres_clctr_min - d1$nbr_genres_clctr_max
+
+
+    d2 <- artnews_df %>%
+        group_by(clctr_name) %>%
+        summarize(nbr_genres = len(unique(genre)))
+        
+    d3 <- as_tibble(merge(d1,d2))
+    d3$diff <- d3$nbr_genres_clctr_max - d3$nbr_genres
+    hist(d3$diff)
+
+readin_artnews_genre()
 
 artnews_sep$cpaer <-
     table(grepl("modern|contempor|minimalism|conceptual|pop|postwar|expressionism|20th|abstract|last 20|", artnews_sep$collection_focus, ignore.case = T))
