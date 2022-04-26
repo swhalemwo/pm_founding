7443cbf55fd3cceb76e2711e31512de024b42040
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-04-26 11:40:54 +0200

    start with brainlet strategies, compare WID and OECD exchange rates (basically match)

--- scripts/cultural_spending.R
+++ scripts/cultural_spending.R
@@ -373,7 +373,125 @@ dev.off()
 
 ## *** series combining
 
+## **** smorc: just calc mean
+un_df2 %>%
+    group_by(iso3c, year, Item, Series) %>%
+    summarize(smorc = mean(Value))
 
+## **** conservative
+un_df2 %>%
+    filter(Series == 1000) %>%
+    select(iso3c, year, Item, caution = Value)
+
+
+names(un_df2)
+names(un_df3)
+
+## **** currency check
+
+un_df2_cur <- un_df2 %>%
+    group_by(iso3c, year) %>%
+    mutate(nbr_curs = len(unique(Currency)))
+
+table(filter(un_df2_cur, nbr_curs > 1)$iso3c)
+
+un_df2_cur %>%
+    ungroup %>%
+    filter(nbr_curs > 1) %>%
+    select(iso3c, Currency)%>%
+    unique()
+
+
+
+table(un_df2_cur$nbr_curs)
+
+filter(un_df2_cur, nbr_curs > 1) %>%
+    select(iso3c, year, Item, Currency) %>%
+    as.data.frame()
+
+## fuck some countries have multiple currencies
+## at least LTU and LVA have euros, and then only BGR left -> can take care of that manually
+
+un_df3$Currency_tws <- trimws(un_df3$Currency)
+
+un_df3_cur <- un_df3 %>%
+    group_by(iso3c, year) %>%
+    mutate(nbr_curs = len(unique(Currency_tws)))
+
+table(un_df3_cur$nbr_curs)
+
+un_df3_cur %>%
+    ungroup() %>%
+    filter(nbr_curs > 1) %>%
+    select(iso3c, Currency_tws) %>%
+    unique() %>%
+    as.data.frame()
+## fuuuuuuuuuuu
+
+
+    
+table(filter(un_df3_cur, nbr_curs > 1)$iso3c)
+## **** currency converter
+
+## WID
+
+currency_cmd <- paste0("select iso3c, year, value from wid_v2 where variable='xlcusx999i' and year>=", STARTING_YEAR)
+wid_currency_df <- as_tibble(dbGetQuery(con, currency_cmd))
+names(wid_currency_df)[3] <- "xlcusx999i"
+
+## OECD
+## oecd_fx_df <- filter_sdmx_results("exchange rates")
+## unique(oecd_fx_df$description.en)
+
+## oecd_str <- "Real effective exchange rates"
+## oecd_str <- "Foreign effective weighted exchange rates"
+## oecd_str <- "EXCH"
+
+## filter(oecd_fx_df, description.en == oecd_str)
+
+## download_oecd_dataset("CSPCUBE", "INTLCOMP_T1")
+## download_oecd_dataset("CSP2012", "INTLCOMP_T1")
+## download_oecd_dataset("CSP2012", "INTLCOMP")
+## ## downloads not working properly 
+## ## EO97_OUTLOOK97: economic outlook 2015
+## ## tbh would be interesting to see the impact of predictions
+
+## maybe just use the website?
+## https://data.oecd.org/conversion/exchange-rates.htm
+
+oecd_cur_df <- as_tibble(read.csv(paste0(PROJECT_DIR, "data/OECD/fx_rates.csv")))
+oecd_cur_df <- select(oecd_cur_df, iso3c=LOCATION, year = TIME, oecd_fx = Value)
+ 
+## comparison
+
+cur_df_cpr <- as_tibble(merge(
+    wid_currency_df,
+    oecd_cur_df))
+   
+cor(cur_df_cpr$xlcusx999i, cur_df_cpr$oecd_fx)
+
+cur_df_sum <- group_by(cur_df_cpr, iso3c) %>%
+    summarize(corx= cor(xlcusx999i, oecd_fx))
+
+hist(cur_df_sum$corx, breaks = 20)
+
+filter(cur_df_sum, corx > 0.9) 
+filter(cur_df_sum, corx < 0.7)
+## maybe can use same UN data source to get GDP? and
+
+cur_df_cpr$region <- countrycode(cur_df_cpr$iso3c, "iso3c", "un.region.name")
+cur_df_cpr <- create_facets(cur_df_cpr, "region", "iso3c", 5)
+
+pdf(paste0(FIG_DIR, "fx_comparison_WID_OECD.pdf"), width = 17, height = 10)
+cur_df_cpr %>%
+    group_by(iso3c) %>%
+    mutate(corx = cor(xlcusx999i, oecd_fx)) %>%
+    ## filter(corx < 0.8) %>%
+    pivot_longer(cols=c("xlcusx999i", "oecd_fx")) %>%
+    ggplot(aes(x=year, y=value, color = iso3c, linetype = name)) +
+    facet_wrap(~iso3c, scales = "free") +
+    geom_line()
+dev.off()
 
 
 ## *** misc 
