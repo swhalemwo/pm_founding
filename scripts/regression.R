12ba13635c01f15af332f633bef0ee0c5f5eeb7f
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-05-11 14:56:44 +0200

    new regression file, starting with generating all the specifications

---
+++ scripts/regression.R
@@ -0,0 +1,9 @@
+all_lngtd_vars <- c("tmitr_approx_linear_2020step",
+                    "hnwi_nbr_30M",
+                    "gptinc992j",
+                    "ghweal992j",
+                    "smorc_dollar_fx",
+                    "NY.GDP.PCAP.CDk",
+                    "SP.POP.TOTLm",
+                    "clctr_cnt_cpaer"
+                    )
