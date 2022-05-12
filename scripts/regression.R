bde20e1c5bf1bad76f830446ebf9ad4405ef994b
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-05-12 10:03:12 +0200

    further functionalizing regression specification generation, not done yet tho

--- scripts/regression.R
+++ scripts/regression.R
@@ -1,3 +1,7 @@
+## * regression
+
+## ** variables and variable combinations (without lagging)
+
 all_lngtd_vars <- c("tmitr_approx_linear_2020step",
                     "hnwi_nbr_30M",
                     "gptinc992j",
@@ -19,36 +23,7 @@ vrbl_cnbs <- list(
     no_cult_spend_and_mitr = all_lngtd_vars[all_lngtd_vars %!in% c("smorc_dollar_fx", "tmitr_approx_linear_2020step")],
     controls = c("NY.GDP.PCAP.CDk", "SP.POP.TOTLm"))
 
-## sketching functionalization
-
-
-hnwi_vars <- sapply(hnwi_cutoff_vlus, \(x) paste0("hnwi_nbr_", sanitize_number(x)))
-inc_ineq_vars <- c("sptinc992j_p90p100", "sptinc992j_p99p100", "gptinc992j")
-weal_ineq_vars <- c("shweal992j_p90p100", "shweal992j_p99p100", "ghweal992j")
-
-
-
-vrbl_choices <- expand.grid(hnwi_var = hnwi_vars,
-            inc_ineq_var = inc_ineq_vars,
-            weal_ineq_var = weal_ineq_vars, stringsAsFactors = F) %>%
-    atb()
-
-## could also just sample here 
-x <- vrbl_choices[1,]
-
-## select variable, generate random lag
-
-lngtd_vars <- c("tmitr_approx_linear_2020step",
-               x$hnwi_var,
-               x$inc_ineq_var,
-               x$weal_ineq_var,
-               "smorc_dollar_fx",
-               "NY.GDP.PCAP.CDk",
-               "SP.POP.TOTLm",
-               "clctr_cnt_cpaer") %>% atb() %>%
-    select(vrbl = value) %>%
-    group_by(vrbl) %>% 
-    mutate(lag = sample(seq(1,5),1))
+## ** sketching functionalization
 
 
 gen_lag <- function(vrbl, lag) {
@@ -64,21 +39,73 @@ gen_lag <- function(vrbl, lag) {
     return(df_lag)
 }
 
+gen_vrbl_thld_choices <- function() {
+    #' generate threshold (hnwi, shares 
+
+    hnwi_vars <- sapply(hnwi_cutoff_vlus, \(x) paste0("hnwi_nbr_", sanitize_number(x)))
+    inc_ineq_vars <- c("sptinc992j_p90p100", "sptinc992j_p99p100", "gptinc992j")
+    weal_ineq_vars <- c("shweal992j_p90p100", "shweal992j_p99p100", "ghweal992j")
+
+
+
+    vrbl_choices <- expand.grid(hnwi_var = hnwi_vars,
+                                inc_ineq_var = inc_ineq_vars,
+                                weal_ineq_var = weal_ineq_vars, stringsAsFactors = F) %>%
+        atb()
+}
+
+
+vrbl_thld_choices <- gen_vrbl_thld_choices()
+
+
+gen_reg_spec <- function() {
+    #' generate the regression specification
+
+    ## could also just sample here, probably have to
+    x <- vrbl_choices[sample(nrow(vrbl_thld_choices),1),]
+
+    ## select variable, generate random lag
+
+    lngtd_vars <- c("tmitr_approx_linear_2020step",
+                    x$hnwi_var,
+                    x$inc_ineq_var,
+                    x$weal_ineq_var,
+                    "smorc_dollar_fx",
+                    "NY.GDP.PCAP.CDk",
+                    "SP.POP.TOTLm",
+                    "clctr_cnt_cpaer") %>% atb() %>%
+        select(vrbl = value) %>%
+        group_by(vrbl) %>% 
+        mutate(lag = sample(seq(1,5),1))
+
+    return(lngtd_vars)
+}
+
+
+
 ## gen_lag(vrbl = "SP.POP.TOTLm", lag=4)
 
-lag_df_list <- apply(lngtd_vars, 1, \(x) gen_lag(vrbl=x[["vrbl"]], lag=x[["lag"]]))
+gen_lag_df <- function(lngtd_vars, crscn_vars, base_vars) {
+    #' generate the data frame given the configuration of variables and lags (lngtd_vars)
 
-## lag TI*TMITR interaction with same lag as TMITR
-lag_df_list[[len(lag_df_list)+1]] <- gen_lag("ti_tmitr_interact", filter(lngtd_vars, vrbl == "tmitr_approx_linear_2020step")$lag)
+    lag_df_list <- apply(lngtd_vars, 1, \(x) gen_lag(vrbl=x[["vrbl"]], lag=x[["lag"]]))
+
+    ## lag TI*TMITR interaction with same lag as TMITR
+    lag_df_list[[len(lag_df_list)+1]] <- gen_lag("ti_tmitr_interact", filter(lngtd_vars, vrbl == "tmitr_approx_linear_2020step")$lag)
 
 
-lag_df <- Reduce(\(x,y) merge(x,y), lag_df_list) %>% atb()
+    lag_df <- Reduce(\(x,y) merge(x,y), lag_df_list) %>% atb()
 
+    df_reg_lags <- atb(merge(df_reg, lag_df))
 
-df_reg_lags <- atb(merge(df_reg, lag_df))
+    ## can skip base vars here, are provided by lag_df
+    ## all_var_names <- c(crscn_vars, names(lag_df))
+    all_var_names <- unique(c(base_vars, crscn_vars, names(lag_df)))
 
-# can skip base vars here, are provided by lag_df
-all_var_names <- c(crscn_vars, names(lag_df))
+    return(df_reg_lags[all_var_names])
+    }
+
+gen_lag_df(lngtd_vars, crscn_vars, base_vars)
 
 ## generate the combinations with a bunch of grepling 
 
