198aa428bea4bfa00f255a2e1d4c2009d383aea3
Johannes Aengenheyster <johannes.ae@hotmail.de>
2022-10-05 13:38:09 +0200

    convert GDP into constant USD, use WID data as source to ensure compatibility with cur_df (based on WID)

--- scripts/wb_api.R
+++ scripts/wb_api.R
@@ -27,16 +27,118 @@ read_WB_data <- function() {
 
 ## df_wb <- read_WB_data()
 
+cpr_gdp_sources <- function(gdp_cvrtd, wid_gdp_cvrtd) {
+    #' compare the GDP sources: WID and WB
+
+    ## calculate metrics to measure differences
+    gdp_cpr <- inner_join(
+        select(gdp_cvrtd, iso3c, year, USD_constant_wb = USD_constant),
+        select(wid_gdp_cvrtd, iso3c, year, USD_constant_wid = USD_constant)) %>%
+        mutate(diff_addgn = USD_constant_wid - USD_constant_wb, # absolute diff
+               diff_mult1 = USD_constant_wid/USD_constant_wb, # relative diff one direction 
+               diff_mult2 = USD_constant_wid/USD_constant_wb) # relative diff other direction
+    ## differences don't seem so big        
+
+    ## aggregate the diffs by country 
+    diff_crys_prep <- gdp_cpr %>%
+        group_by(iso3c) %>%
+        summarize(sum_diff_addgns = sum(diff_addgn)/len(diff_addgn),
+                  sum_diff_mult1 = sum(diff_mult1)/len(diff_mult1),
+                  sum_diff_mult2 = sum(diff_mult2)/len(diff_mult2))
+
+    ## select all the countries that are in top 30 on any of metrics (atm 34)
+    diff_crys_vec <- lapply(c("sum_diff_addgns", "sum_diff_mult1", "sum_diff_mult2"), \(x)
+                            diff_crys_prep %>% arrange(-get(x)) %>% head(n=30) %>% pull(iso3c)) %>%
+        unlist() %>% unique()
+    
+    ## plot gdp lines, color by source
+    bind_rows(
+        select(gdp_cvrtd, iso3c, year, USD_constant) %>% mutate(source = "WB"),
+        select(wid_gdp_cvrtd, iso3c, year, USD_constant) %>% mutate(source = "WID")) %>%
+        filter(iso3c %in% diff_crys_vec) %>% 
+        ggplot(aes(x=year, y=USD_constant, group = source, color = source)) +
+        geom_line() +
+        facet_wrap(~iso3c, scales = "free")
+    ## invisible(NULL)
+
+    
+}
+    
+get_wid_gdp <- function() {
+    #' get the gdp per capita values from WID 
+
+    ## use agdpro999i: constant LCU per capita
+    wid_gdp_cmd <-sprintf(
+        "select iso3c, year, variable, value from wid_v2 where variable = 'agdpro999i' AND year >= %s",
+        STARTING_YEAR)
+
+    wid_gdp_tb <- atb(dbGetQuery(con, wid_gdp_cmd))
+    names(wid_gdp_tb) <- c("iso3c", "year", "gdp_var", "gdp_vlu")
+    
+    wid_gdp_prep <- inner_join(wid_gdp_tb,
+               select(cur_df, iso3c, year, xlcusx999i_2021))
+    
+    ## convert: WID is in constant LCU -> can just convert with xlcusx999i_2021 into constant USD
+    wid_gdp_cvrtd <- wid_gdp_prep %>%
+        mutate(USD_constant = gdp_vlu/xlcusx999i_2021) %>%
+        mutate(region = countrycode(iso3c, "iso3c", "un.regionsub.name"))
+
+    return(wid_gdp_cvrtd)
+}
+
+CPR_GDP_SOURCES <- F
+
+cvrt_ny.gdp.pcap.cd <- function(df_wb_new) {
+    #' wrapper for conversion of GDP into constant USD
+
+    ## join with cur_df to convert to 2021-constant USD
+    gdp_prep <- inner_join(select(df_wb_new, iso3c, year, NY.GDP.MKTP.CN, SP.POP.TOTL),
+                           select(cur_df, iso3c, year, inyixx999i, xlcusx999i_2021)) 
+
+    ## use World bank data 
+    gdp_cvrtd <- gdp_prep %>%
+        mutate(LCU_current_pcap = NY.GDP.MKTP.CN/SP.POP.TOTL) %>% # get gdp pcap current lcu
+        mutate(LCU_constant_pcap = LCU_current_pcap/inyixx999i) %>% # convert everything to constant LCU
+        mutate(USD_constant = LCU_constant_pcap/xlcusx999i_2021) %>% # get constant USD
+        mutate(region = countrycode(iso3c, "iso3c", "un.region.name"))
+
+    ## filter(gdp_cvrtd, iso3c == "DEU")
+
+    ## viz_lines(gdp_cvrtd, y="USD_constant", facets = "region")
+    ## hmm Venezuela is bs, rest could be plausible? idk
+
+    ## use WID data
+    wid_gdp_cvrtd <- get_wid_gdp()
+    
+    ## convert gdp of WB and WID if necessary; atm doesn't seem like big difference
+    if (CPR_GDP_SOURCES) {cpr_gdp_sources(gdp_cvrtd, wid_gdp_cvrtd)}
+
+    df_wb_new2 <- inner_join(df_wb_new, select(wid_gdp_cvrtd, iso3c, year, USD_constant))
+
+    df_wb_new3 <- df_wb_new2 %>%
+        select(-NY.GDP.PCAP.CD) %>%
+        select(everything(), NY.GDP.PCAP.CD = USD_constant)
+
+    return(df_wb_new3)
+}
+
+        
+
+    
+
+
+
 
 
 get_WB_data <- function(indx, refresh_all=FALSE) {
+    if (as.character(match.call()[[1]]) %in% fstd){browser()}
     #' get WB variables, download all new if asked to do so, else read from file
 
     ## download everything if no file is there
     if ("WB_data_proc.csv" %in% list.files(PROC_DATA_DIR)) {
         df_wb_local <- read_WB_data()
         wb_vars_there <- names(df_wb_local)[4:len(names(df_wb_local))]
-        print(paste0("wb_vars_there: ", wb_vars_there))
+        print(paste0("wb_vars_there: ", paste0(wb_vars_there, collapse = ", ")))
 
     } else {
         nothing_there <- TRUE
@@ -58,8 +160,8 @@ get_WB_data <- function(indx, refresh_all=FALSE) {
         
     } else {
         wb_vars_to_download <- indx[which(indx %!in% wb_vars_there)]
-        print(length(wb_vars_to_download))
-        print(paste0("download selected indicator(s) anew: ", wb_vars_to_download)) #
+        print(paste0("number of vars to download: ", length(wb_vars_to_download)))
+        print(paste0("download selected indicator(s) anew: ", wb_vars_to_download))
         
         if (length(wb_vars_to_download) > 0) {
             df_wb_api <- download_WB_data(wb_vars_to_download)
@@ -68,16 +170,27 @@ get_WB_data <- function(indx, refresh_all=FALSE) {
         }
 
         unchanged_vars <- wb_vars_there[which(wb_vars_there %!in% wb_vars_to_download)]
-        print(paste0("unchanged_vars: ", unchanged_vars))
+        print(paste0("unchanged_vars: ", paste0(unchanged_vars, collapse = ", ")))
         ## need to this out: how to get the variables which shouldn't be changed; think i got it now
     }
 
     df_wb_new <- as_tibble(merge(df_wb_local[,c("iso3c", "country", "year", unchanged_vars)], df_wb_api))
+
+    ## inspect coverage 
+    ## colSums(is.na(select(df_wb_new, NY.GDP.PCAP.CD, NY.GDP.MKTP.CN)))
+
+    ## convert to constant USD, using WID data
+    ## actually also needs NY.GDP.MKTP.CN to be present for comparison.. could be yeeted tho
+    if ("NY.GDP.PCAP.CD" %in% indx) {
+        df_wb_new <- cvrt_ny.gdp.pcap.cd(df_wb_new)
+    }
+
     save_WB_data(df_wb_new)
 
     return(df_wb_new[,c("iso3c", "country", "year", indx)])
     
 }
+
 
 
 ## indx <- c("NY.GDP.PCAP.CD")
