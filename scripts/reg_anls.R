0ada425589c9aec4168f4e0bd82206ca3075836f
Johannes Aengenheyster <johannes.ae@hotmail.de>
2023-06-09 10:37:32 +0200

    next version (v88)

--- scripts/reg_anls.R
+++ scripts/reg_anls.R
@@ -3008,7 +3008,7 @@ stop("functions done")
 stylecfg <- list(lbl_fntsz = 9)
 
 NBR_MDLS <- 1
-batch_version <- "v83"
+batch_version <- "v88"
 ## fldr_info <- fldr_info_optmz
 fldr_info <- setup_regression_folders_and_files(batch_version)
 reg_anls_base <- read_reg_anls_files(fldr_info)
