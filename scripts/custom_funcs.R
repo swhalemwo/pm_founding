63f68e2f5d66b7b50b50d4e86defb1c9b73a331b
swhalemwo <jojo.ae@hotmail.de>
2024-01-31 17:17:05 +0100

    copy all kind of jtls code here

--- scripts/custom_funcs.R
+++ scripts/custom_funcs.R
@@ -860,3 +860,104 @@ color_stretcher <- function(col_vec, midpt, minpt, maxpt, stretch, sharp_edge) {
     return(col_vec_cbnd)
 }
 sumry <- summary
+
+
+nicely_fmt_number <- function(vlu, max_digits = 3) {
+    ## if (as.character(match.call()[[1]]) %in% fstd){browser()}
+    #' round number to nice looking number of decimal places
+    #' depends on size: each order of magnitude reduces decimal digits by one
+    #' max_digits: how many digits there can be at most
+
+    nbr_digits_before_comma <- floor(log10(abs(vlu)))
+    
+    ## need to use convoluted if-block because switch only allows integer/string matching
+    if (nbr_digits_before_comma < 0) {rnd <- max_digits
+    } else if (nbr_digits_before_comma == 0) {rnd <- max_digits - 1
+    } else if (nbr_digits_before_comma == 1) {rnd <- max_digits - 2
+    } else if (nbr_digits_before_comma > 1) {rnd <- max_digits - 3
+    }
+
+    ## add some special cases for 0 and 1: don't need decimal places
+    ## if (vlu %!in% c(0,1)) {
+    if (vlu %% 1 == 0) { # integer check: don't need decimal points
+        format(vlu, nsmall = 0, big.mark = " ")
+    } else {
+        format(round(vlu, rnd), big.mark = " ", nsmall = max(0,rnd)) # nsmall can't be lower than 0
+        
+    }
+}
+
+#' first try at more automatic decimal points formatting
+#'
+#' @export
+nicely_fmt_number_v <- Vectorize(nicely_fmt_number)
+
+
+
+rcd_iso3c_reg6 <- function(iso3cs) {
+    #' custom recoding to PMDB region scheme of 6 continents (NA, LA, EU, AF, AS, OC)
+    ## if (as.character(match.call()[[1]]) %in% fstd){browser()}
+    ## 1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;
+
+    regs_unreg <- countrycode(iso3cs, "iso3c", "un.region.name", custom_match = c("TWN"= "Asia"))
+    regs_unregsub <- countrycode(iso3cs ,"iso3c", "un.regionsub.name", custom_match = c("TWN"= "Asia"))
+
+    ## split americas into north and south
+    locs_americas <- which(regs_unregsub %in% c("Latin America and the Caribbean", "Northern America"))
+    regs_unreg[locs_americas] <- regs_unregsub[locs_americas]
+
+    ## custom recoding to PMDB region names 
+
+    reg6_lbls <<- list(
+        EU = "Europe",
+        AS = "Asia",
+        AF = "Africa",
+        NALUL = "North America",
+        LA = "Latin America",
+        OC = "Oceania")
+
+
+    reg_cbn_rcd_list <- list(
+        Europe = "EU",
+        Asia = "AS",
+        Africa = "AF",
+        "Northern America" = "NALUL", # avoid collision with NA
+        "Latin America and the Caribbean" = "LA",
+        "Oceania" = "OC")
+
+    rcd_iso3c_reg6_sub <- function(reg, reg_cbn_rcd_list) {
+        #' individual handling of reg6 coding: return NA reg not matched
+        if (reg %in% names(reg_cbn_rcd_list)) {
+            reg_cbn_rcd_list[[reg]]
+        } else {
+            NA
+        }
+    }
+
+    map_chr(regs_unreg, ~rcd_iso3c_reg6_sub(.x, reg_cbn_rcd_list))
+
+}
+
+
+fmt_nbr_flex_single <- function(vlu, digits = 3) {
+    #' flexibly format number
+    #' if integer: no decimal points
+    #' if not integer, round to digits
+    #' big numbers always split by space
+
+    
+    if (vlu %% 1 == 0) { # integer check: don't need decimal points
+        format(vlu, nsmall = 0, big.mark = " ")
+    } else {
+        format(round(vlu, digits), big.mark = " ", nsmall = digits) # nsmall can't be lower than 0
+        
+    }
+}
+
+
+#' formats number 
+#'
+#' might not be so relevant: maybe ok descriptive tables,
+#' but in-text numbers seem to require hand-determined number of decimal points
+#' @export
+fmt_nbr_flex <- Vectorize(fmt_nbr_flex_single)
