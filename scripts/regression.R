642c8a5a32e242528be375885749d9a15a27fd60
swhalemwo <jojo.ae@hotmail.de>
2024-02-01 09:59:47 +0100

    full model test

--- scripts/regression.R
+++ scripts/regression.R
@@ -16,13 +16,13 @@ vrbl_thld_choices_optmz <- slice_sample(vrbl_thld_choices, n=36)
 reg_settings_optmz <- list(
     nbr_specs_per_thld = 2,
     dvfmts = c("rates"), # should also be counts, but multiple dvfmts not yet supported by reg_anls
-    batch_version = "v19",
+    batch_version = "v20",
     lags = 1:5,
     vary_vrbl_lag = F,
     technique_strs = c("nr"),
     difficulty_switches = T,
     regcmds = c("glmmTMB"),
-    cbns_to_include = names(cbn_df_dict$counts)[1],
+    cbns_to_include = names(cbn_df_dict$counts)[1:3],
     mdls_to_include = c("full"),
     wtf = T,
     max_loop_nbr = 100
