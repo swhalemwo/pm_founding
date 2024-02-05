67e3ae326e80ea6384bd0381b35a08bea957bd7f
swhalemwo <jojo.ae@hotmail.de>
2024-02-05 09:40:21 +0100

    automate installation of system packages and R libraries

---
+++ scripts/setup_surf.R
@@ -0,0 +1,17 @@
+
+
+syspkgs <- c("cmake", "libxml2-dev", "libssl-dev", "libcurl4-openssl-dev", "libfontconfig1-dev")
+
+rlibs <- c("data.table", "dplyr", "purrr", "glmmTMB", "ggplot2", "parallel",
+           "docstring", "DBI", "rsdmx", "collapse", "modelsummary", "RSQLite",
+           "xtable", "Hmisc", "furrr", "performance", "tidyr", "dineq",
+           "stringr", "ggbeeswarm", "patchwork", "ggridges", "ggrepel",
+           "texreg", "countrycode")
+
+cmd_syspkgs <- sprintf("sudo apt-get install %s", paste0(syspkgs, collapse = " "))
+system(cmd_syspkgs)
+
+install.packages(r_libs)
+
+
+                     
