1b0b227d50033dd99ff0ac27f972e394022301d4
swhalemwo <jojo.ae@hotmail.de>
2024-02-05 10:25:08 +0100

    update surf setup script

--- scripts/setup_surf.R
+++ scripts/setup_surf.R
@@ -1,6 +1,14 @@
+# * setup script for surf
 
+## ** install system packages necessary for R packages
 
 syspkgs <- c("cmake", "libxml2-dev", "libssl-dev", "libcurl4-openssl-dev", "libfontconfig1-dev")
+
+
+cmd_syspkgs <- sprintf("sudo apt-get install %s", paste0(syspkgs, collapse = " "))
+system(cmd_syspkgs)
+
+## ** install R packages
 
 rlibs <- c("data.table", "dplyr", "purrr", "glmmTMB", "ggplot2", "parallel",
            "docstring", "DBI", "rsdmx", "collapse", "modelsummary", "RSQLite",
@@ -8,11 +16,26 @@ rlibs <- c("data.table", "dplyr", "purrr", "glmmTMB", "ggplot2", "parallel",
            "stringr", "ggbeeswarm", "patchwork", "ggridges", "ggrepel",
            "texreg", "countrycode")
 
-cmd_syspkgs <- sprintf("sudo apt-get install %s", paste0(syspkgs, collapse = " "))
-system(cmd_syspkgs)
+## check which packages are installed already
+rlib_status <- sapply(rlibs, \(x) require(x, character.only = T))
 
-install.packages(rlibs,
+## install those that aren't installed yet 
+install.packages(rlibs[!rlib_status],
                  lib = "/home/jaengenhey/R/x86_64-pc-linux-gnu-library/4.1")
+
+
+## check that packages have been installed
+rlib_status2 <- sapply(rlibs, \(x) require(x, character.only = T))
+if (any(!rlib_status2)) {
+    stop(sprintf("packages %s not installed",
+                 paste0(rlibs[!rlib_status2], collapse = ", "))}
+
+## pckgs_test <- c("data.table", "kapparino", "dplyr")
+## lapply(pckgs_test, \(x) library(x, character.only = T))
+
+
+
+
 
 
                      
