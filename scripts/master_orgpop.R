8a97a660973e16ed50e6a90c615d83d266023257
swhalemwo <jojo.ae@hotmail.de>
2024-02-06 10:46:02 +0100

    fix paths

--- scripts/master_orgpop.R
+++ scripts/master_orgpop.R
@@ -6,7 +6,7 @@ PROJECT_DIR <- gsub("org_pop/scripts", "org_pop/", x = getwd())
 library(collapse) # needed for reading startup_static
 
 
-SCRIPT_DIR <- paste0(PROJ_DIR, "scripts/")
+SCRIPT_DIR <- paste0(PROJECT_DIR_DIR, "scripts/")
 
 source(paste0(SCRIPT_DIR, "startup_static.R"))
 
@@ -14,10 +14,10 @@ LOG_DIR <- paste0(REG_MONKEY_DIR, reg_settings_optmz$batch_version, "/logs/")
 if (!dir.exists(LOG_DIR)) {dir.create(LOG_DIR, recursive = T)}
 
 ## define commands
-cmd_reg            <- sprintf("Rscript regression.R %s >& %slog_reg.txt"     , PROJ_DIR, LOG_DIR)
-cmd_postestimation <- sprintf("Rscript postestimation.R %s >& %slog_post.txt", PROJ_DIR, LOG_DIR)
-cmd_anls           <- sprintf("Rscript reg_anls.R %s >& %slog_anls.txt"      , PROJ_DIR, LOG_DIR)
-cmd_finalize       <- sprintf("Rscript finalize.R %s >& %slog_finalize.txt"  , PROJ_DIR, LOG_DIR)
+cmd_reg            <- sprintf("Rscript regression.R %s >& %slog_reg.txt"     , PROJECT_DIR, LOG_DIR)
+cmd_postestimation <- sprintf("Rscript postestimation.R %s >& %slog_post.txt", PROJECT_DIR, LOG_DIR)
+cmd_anls           <- sprintf("Rscript reg_anls.R %s >& %slog_anls.txt"      , PROJECT_DIR, LOG_DIR)
+cmd_finalize       <- sprintf("Rscript finalize.R %s >& %slog_finalize.txt"  , PROJECT_DIR, LOG_DIR)
 
 ## run commands
 status_reg   <- system(cmd_reg)
