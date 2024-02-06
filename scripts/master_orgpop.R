e0abd84d908cfb51f87c76c6ae73d6a106e16e25
swhalemwo <jojo.ae@hotmail.de>
2024-02-06 10:58:02 +0100

    enable command logging on sh

--- scripts/master_orgpop.R
+++ scripts/master_orgpop.R
@@ -14,10 +14,10 @@ LOG_DIR <- paste0(REG_MONKEY_DIR, reg_settings_optmz$batch_version, "/logs/")
 if (!dir.exists(LOG_DIR)) {dir.create(LOG_DIR, recursive = T)}
 
 ## define commands
-cmd_reg            <- sprintf("Rscript regression.R %s >& %slog_reg.txt"     , PROJECT_DIR, LOG_DIR)
-cmd_postestimation <- sprintf("Rscript postestimation.R %s >& %slog_post.txt", PROJECT_DIR, LOG_DIR)
-cmd_anls           <- sprintf("Rscript reg_anls.R %s >& %slog_anls.txt"      , PROJECT_DIR, LOG_DIR)
-cmd_finalize       <- sprintf("Rscript finalize.R %s >& %slog_finalize.txt"  , PROJECT_DIR, LOG_DIR)
+cmd_reg            <- sprintf("Rscript regression.R %s > %slog_reg.txt 2>&1"     , PROJECT_DIR, LOG_DIR)
+cmd_postestimation <- sprintf("Rscript postestimation.R %s > %slog_post.txt 2>&1", PROJECT_DIR, LOG_DIR)
+cmd_anls           <- sprintf("Rscript reg_anls.R %s > %slog_anls.txt 2>&1"      , PROJECT_DIR, LOG_DIR)
+cmd_finalize       <- sprintf("Rscript finalize.R %s > %slog_finalize.txt 2>&1"  , PROJECT_DIR, LOG_DIR)
 
 ## run commands
 status_reg   <- system(cmd_reg)
