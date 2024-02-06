0a61c136eaa80fe01c6d1b0f1d3230b76d427b57
swhalemwo <jojo.ae@hotmail.de>
2024-02-06 10:42:07 +0100

    rewrite maste_orgpop to add logging, better error handling

--- scripts/master_orgpop.R
+++ scripts/master_orgpop.R
@@ -1,18 +1,34 @@
 ## * master script for org_pop
 
 ## hacky way to get PROJ DIR
-PROJ_DIR <- gsub("org_pop/scripts", "org_pop/", x = getwd())
+PROJECT_DIR <- gsub("org_pop/scripts", "org_pop/", x = getwd())
 
+library(collapse) # needed for reading startup_static
+
+
+SCRIPT_DIR <- paste0(PROJ_DIR, "scripts/")
+
+source(paste0(SCRIPT_DIR, "startup_static.R"))
+
+LOG_DIR <- paste0(REG_MONKEY_DIR, reg_settings_optmz$batch_version, "/logs/")
+if (!dir.exists(LOG_DIR)) {dir.create(LOG_DIR, recursive = T)}
 
 ## define commands
-cmd_reg            <- sprintf("Rscript regression.R %s"    , PROJ_DIR)
-cmd_postestimation <- sprintf("Rscript postestimation.R %s", PROJ_DIR)
-cmd_anls           <- sprintf("Rscript reg_anls.R %s"      , PROJ_DIR)
-cmd_finalize       <- sprintf("Rscript finalize.R %s"      , PROJ_DIR)
+cmd_reg            <- sprintf("Rscript regression.R %s >& %slog_reg.txt"     , PROJ_DIR, LOG_DIR)
+cmd_postestimation <- sprintf("Rscript postestimation.R %s >& %slog_post.txt", PROJ_DIR, LOG_DIR)
+cmd_anls           <- sprintf("Rscript reg_anls.R %s >& %slog_anls.txt"      , PROJ_DIR, LOG_DIR)
+cmd_finalize       <- sprintf("Rscript finalize.R %s >& %slog_finalize.txt"  , PROJ_DIR, LOG_DIR)
 
 ## run commands
-system(cmd_reg)
-system(cmd_postestimation)
-system(cmd_anls)
-system(cmd_finalize)
+status_reg   <- system(cmd_reg)
+if (status_reg != 0) {stop("error in cmd_reg")}
+
+status_post  <- system(cmd_postestimation)
+if (status_post != 0) {stop("error in cmd_postestimation")}
+
+status_anls  <- system(cmd_anls)
+if (status_anls != 0) {stop("error in cmd_anls")}
+
+status_final <- system(cmd_finalize)
+if (status_final != 0) {stop("error in cmd_final")}
        
