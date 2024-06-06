770239b3bef06bebf3ae855b6186e99c58a20177
swhalemwo <jojo.ae@hotmail.de>
2024-06-06 10:33:23 +0200

    modify reg settings format to switch comfily between local and remote

--- scripts/startup_static.R
+++ scripts/startup_static.R
@@ -82,18 +82,19 @@ reg_settings_optmz <- list(
     ## relevant parameters
     ## uncomment only one of these
     ## on laptop
-    ## batch_version       = "v30",   
-    ## nbr_specs_per_thld  =  1,      
-    ## lags                =  1:3,    
-    ## cbns_to_include     =  "cbn1", 
-    ## n_vrbl_thld_choices =  4,      
-    ## for surf
-    batch_version       = "v31",         
-    nbr_specs_per_thld  = 4,                 
-    lags                = 1:5,               
-    cbns_to_include     = paste0("cbn", 1:3),
-    n_vrbl_thld_choices = 36,                
-    ## no longer varying
+    ## ---------------
+    batch_version       = "v32",   
+    nbr_specs_per_thld  =  1,      
+    lags                =  1:3,    
+    cbns_to_include     =  "cbn1", 
+    n_vrbl_thld_choices =  4,      
+    ## -------------- for surf
+    ## batch_version       = "v31",         
+    ## nbr_specs_per_thld  = 4,                 
+    ## lags                = 1:5,               
+    ## cbns_to_include     = paste0("cbn", 1:3),
+    ## n_vrbl_thld_choices = 36,
+    ## ---------------- no longer varying
     dvfmts              = c("rates"), 
     vary_vrbl_lag       = F,
     technique_strs      = c("nr"),
