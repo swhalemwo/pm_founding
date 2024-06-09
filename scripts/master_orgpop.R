3e369bb1f879128d03cc32d3f2edd936c082b24e
swhalemwo <jojo.ae@hotmail.de>
2024-06-09 19:03:10 +0200

    temporarily uncomment reg and postest commands for surf server

--- scripts/master_orgpop.R
+++ scripts/master_orgpop.R
@@ -20,12 +20,12 @@ cmd_anls           <- sprintf("Rscript reg_anls.R %s > %slog_anls.txt 2>&1"     
 cmd_finalize       <- sprintf("Rscript finalize.R %s > %slog_finalize.txt 2>&1"  , PROJECT_DIR, LOG_DIR)
 
 ## run commands
-status_reg   <- system(cmd_reg)
-print(cmd_reg)
-if (status_reg != 0) {stop("error in cmd_reg")}
+## status_reg   <- system(cmd_reg)
+## print(cmd_reg)
+## if (status_reg != 0) {stop("error in cmd_reg")}
 
-status_post  <- system(cmd_postestimation)
-if (status_post != 0) {stop("error in cmd_postestimation")}
+## status_post  <- system(cmd_postestimation)
+## if (status_post != 0) {stop("error in cmd_postestimation")}
 
 status_anls  <- system(cmd_anls)
 if (status_anls != 0) {stop("error in cmd_anls")}
